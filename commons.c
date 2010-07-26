482affb9b972be3b34605bac8ffc7e7aba030caa
Gerardo Orellana <goaccess@prosoftcorp.com>
2010-07-26 12:17:52 -0500

    Unify init_pair() routines

--- commons.c
+++ commons.c
@@ -3,7 +3,7 @@
  * Copyright (C) 2010 by Gerardo Orellana <goaccess@prosoftcorp.com>
  * GoAccess - An ncurses apache weblog analyzer & interactive viewer
  * @version 0.2
- * Last Modified: Saturday, July 10, 2010
+ * Last Modified: Sunday, July 25, 2010
  * Path:  /commons.c
  *
  * This program is distributed in the hope that it will be useful,
@@ -18,14 +18,27 @@
  * Visit http://goaccess.prosoftcorp.com for new releases.
  */
 
+#include <curses.h>
 #include <time.h>
 #include <glib.h>
 
 #include "commons.h"
 
+#define COL_WHITE    0
+#define COL_BLUE     1
+#define COL_GREEN    11
+#define COL_RED      3
+#define COL_BLACK    4
+#define COL_CYAN     5
+#define COL_YELLOW   6
+#define BLUE_GREEN   7
+#define BLACK_GREEN  8
+#define BLACK_CYAN   9
+#define WHITE_RED    10
+
 /* Definitions checked against declarations */
-GHashTable *ht_unique_visitors 	= NULL;
-GHashTable *ht_requests = NULL; 
+GHashTable *ht_unique_visitors = NULL;
+GHashTable *ht_requests = NULL;
 GHashTable *ht_referers = NULL;
 GHashTable *ht_unique_vis = NULL;
 GHashTable *ht_requests_static = NULL;
@@ -35,6 +48,7 @@ GHashTable *ht_browsers = NULL;
 GHashTable *ht_hosts = NULL;
 GHashTable *ht_status_code = NULL;
 GHashTable *ht_referring_sites = NULL;
+GHashTable *ht_keyphrases = NULL;
 
 enum MODULES modules;
 
@@ -64,24 +78,25 @@ size_t real_size_y = 0;
 /* file */
 char *ifile = NULL;
 
-char *module_names[] 	= { 
-	"  Visitors - ^s^ sort by date - ^S^ sort by hits", 
-	"  Requests", "  Static Requests", "  Referrers", 
-	"  404 - Not Found", "  Operating Systems ordered by unique hits", 
-	"  Browsers ordered by unique hits", "  Hosts", 
-	"  HTTP Status Codes", "  Top Referring Sites" 
+char *module_names[] = {
+    "  Visitors - ^s^ sort by date - ^S^ sort by hits",
+    "  Requests", "  Static Requests", "  Referrers",
+    "  404 - Not Found", "  Operating Systems ordered by unique hits",
+    "  Browsers ordered by unique hits", "  Hosts",
+    "  HTTP Status Codes", "  Top Referring Sites",
+    "  Top Search Keyphrases (Google, Cache, Translate)"
 };
 
-const char *os[] 		= { 
-	"Windows", "Debian", "Ubuntu", "Fedora", "Android", "Centos", 
-	"Gentoo", "Linux", "linux", "iPad", "Macintosh", "BSD", "SunOS", 
-	"iPhone", "Amiga", "BlackBerry", "SymbianOS", 
-	"PlayStation Portable", "DoCoMo" 
+const char *os[] = {
+    "Windows", "Debian", "Ubuntu", "Fedora", "Android", "Centos",
+    "Gentoo", "Linux", "linux", "iPad", "Macintosh", "BSD", "SunOS",
+    "iPhone", "Amiga", "BlackBerry", "SymbianOS",
+    "PlayStation Portable", "DoCoMo"
 };
 
-const char *browsers[]  = {
+const char *browsers[] = {
     "MSIE 6.0", "MSIE 7.0", "MSIE 8.0", "MSIE",
-    "Firefox", "Netscape", "SVN", "Opera"
+    "Firefox", "Netscape", "SVN", "Opera",
     "Chrome", "Safari", "Konqueror", "Media Center PC",
     "Wget", "w3m", "Links", "Lynx",
     "Camino", "Dillo", "Kazehakase", "msnbot-media",
@@ -91,121 +106,142 @@ const char *browsers[]  = {
     "Iceape", "Nokia", "Donzilla", "Kazehakase",
     "Galeon", "Exabot", "K-Meleon", "Avant",
     "curl", "archive.org_bot", "BrowseX", "ELinks",
-    "Iceweasel","Google Wireless Transcoder", "Google-Sitemaps",
+    "Iceweasel", "Google Wireless Transcoder", "Google-Sitemaps",
     "Mosaic", "Songbird", "W3C_Validator", "Jigsaw",
     "Flock", "Minefield", "Midori", "Opera Mini"
 };
 
-char *codes[][2] = 
-{
-	{ "100", "Continue - Server has received the request headers" },
-	{ "101", "Switching Protocols - Requester has asked the server to switch protocols" },
-	{ "200", "OK -  Successful HTTP requests" },
-	{ "201", "Created -  The request has been fulfilled and created" },
-	{ "202", "Accepted - The request has been accepted for processing" },
-	{ "203", "Non-Authoritative Information" },
-	{ "204", "No Content - Request is not returning any content" },
-	{ "205", "Reset Content - Response requires requester reset the document view" },
-	{ "206", "Partial Content - The server is delivering only part of the resource" },
-	{ "300", "Multiple Choices - Multiple options for the resource" },
-	{ "301", "Moved Permanently - Future requests should be directed to the given URI" },
-	{ "302", "302 Found - Moved Temporarily" },
-	{ "303", "303 See Other - Response to the request can be found under another URI" },
-	{ "304", "Not Modified - Resource has not been modified since last requested" },
-	{ "305", "Use Proxy - HTTP/1.1" },
-	{ "307", "Temporary Redirect - Request should be repeated with another URI" },
-	{ "400", "Bad Request - Request contains bad syntax or cannot be fulfilled" },
-	{ "401", "Unauthorized - Authentication has failed or not yet been provided" },
-	{ "402", "Payment Required" },
-	{ "403", "Forbidden -  Server is refusing to respond to it" },
-	{ "404", "Not Found - Requested resource could not be found" },
-	{ "405", "Method Not Allowed - Request method not supported by that resource" },
-	{ "406", "Not Acceptable - " },
-	{ "407", "Proxy Authentication Required" },
-	{ "408", "Request Timeout - The server timed out waiting for the request" },
-	{ "409", "Conflict - Conflict in the request" },
-	{ "410", "Gone - Resource requested is no longer available" },
-	{ "411", "Length Required - Request did not specify the length of its content" },
-	{ "412", "Precondition Failed - Server does not meet one of the preconditions" },
-	{ "413", "Request Entity Too Large - request is larger than the server is able to process" },
-	{ "414", "Request-URI Too Long - The URI provided was too long for the server to process" },
-	{ "415", "Unsupported Media Type - Media type which the server or resource does not support" },
-	{ "416", "Requested Range Not Satisfiable - Cannot supply that portion" },
-	{ "417", "Expectation Failed" },
-	{ "500", "Internal Server Error" },
-	{ "502", "Bad Gateway - Received an invalid response from the upstream server" },
-	{ "503", "Service Unavailable - The server is currently unavailable" },
-	{ "504", "Gateway Timeout - Did not receive a timely request from the upstream server" },
-	{ "505", "HTTP Version Not Supported" }
+char *codes[][2] = {
+    {"100", "Continue - Server has received the request headers"},
+    {"101", "Switching Protocols - Client asked to switch protocols"},
+    {"200", "OK -  The request sent by the client was successful"},
+    {"201", "Created -  The request has been fulfilled and created"},
+    {"202", "Accepted - The request has been accepted for processing"},
+    {"203", "Non-Authoritative Information"},
+    {"204", "No Content - Request is not returning any content"},
+    {"205", "Reset Content - User-Agent should reset the document"},
+    {"206", "Partial Content - The partial GET has been successful"},
+    {"300", "Multiple Choices - Multiple options for the resource"},
+    {"301", "Moved Permanently - Resource has permanently moved"},
+    {"302", "302 Found - Moved Temporarily"},
+    {"303", "303 See Other - The response is at a different URI"},
+    {"304", "Not Modified - Resource has not been modified"},
+    {"305", "Use Proxy - Can only be accessed through the proxy"},
+    {"307", "Temporary Redirect - Resource temporarily moved"},
+    {"400", "Bad Request - The syntax of the request is invalid"},
+    {"401", "Unauthorized - Request needs user authentication"},
+    {"402", "Payment Required"},
+    {"403", "Forbidden -  Server is refusing to respond to it"},
+    {"404", "Not Found - Requested resource could not be found"},
+    {"405", "Method Not Allowed - Request method not supported"},
+    {"406", "Not Acceptable"},
+    {"407", "Proxy Authentication Required"},
+    {"408", "Request Timeout - The server timed out waiting for the request"},
+    {"409", "Conflict - Conflict in the request"},
+    {"410", "Gone - Resource requested is no longer available"},
+    {"411", "Length Required - Invalid Content-Length"},
+    {"412", "Precondition Failed - Server does not meet preconditions"},
+    {"413", "Request entity Too Long"},
+    {"414", "Request-URI Too Long"},
+    {"415", "Unsupported Media Type - Media type is not supported"},
+    {"416", "Requested Range Not Satisfiable - Cannot supply that portion"},
+    {"417", "Expectation Failed"},
+    {"500", "Internal Server Error"},
+    {"502", "Bad Gateway - Received an invalid response from the upstream"},
+    {"503", "Service Unavailable - The server is currently unavailable"},
+    {"504", "Gateway Timeout - The upstream server failed to send request"},
+    {"505", "HTTP Version Not Supported"}
 };
 
 char *help_main[] = {
-	"Copyright (C) 2010",
-	"by Gerardo Orellana <goaccess@prosoftcorp.com>",
-	"http://goaccess.prosoftcorp.com",
-	"Released under the GNU GPL. See `man` page for",
-	"more details.",
-	"",
-	"GoAccess is an open source Apache web log analyzer",
-	"that provides fast and valuable HTTP statistics",
-	"for system administrators that require a visual",
-	"report on the fly.",
-	"",
-	"The data collected based on the parsing of the log",
-	"is divided into different modules. Modules are",
-	"automatically generated and presented to the user.",
-	"",  
-	"The main window displays general statistics, top ",
-	"visitors, requests, browsers, operating systems,",
-	"hosts, etc.",
-	"",
-	"The user can make use of the following keys:",
-	" ^F1^    main help",
-	" ^q^     quit the program/current window",
-	" ^o^     open detail view for selected module",
-	" ^TAB^   iterate modules (forward)",
-	" ^SHIFT^ + ^TAB^ iterate modules (backward)",
-	" ^RIGHT ARROW^ open detail view for selected",
-	"         module",
-	" ^0-9^   select module so the user can open a",
-	"         [detail view] with either ^o^ or", 
-	"         ^RIGHT ARROW^",
-	" ^s^     [detail view] sort unique visitors by date",
-	" ^S^     [detail view] sort unique visitors by hits",
-	" ^/^     [detail view] search forward for the ",
-	"         occurrence of pattern",
-	" ^n^     detail view - find the position of the next",
-	"         occurrence",
-	" ^t^     [detail view] move to the first item",
-	" ^b^     [detail view] move to the last item",
-	"",
-	"If you believe you have found a bug, please drop me",
-	"an email with details.",
-	"",
-	"If you have a medium or high traffic website, it",
-	"would be interesting to hear your experience with",
-	"GoAccess, such as generating time, visitors or hits.",
-	"",
-	"Send your questions, comments and suggestions to ",
-	"goaccess@prosoftcorp.com",
+    "Copyright (C) 2010",
+    "by Gerardo Orellana <goaccess@prosoftcorp.com>",
+    "http://goaccess.prosoftcorp.com",
+    "Released under the GNU GPL. See `man` page for",
+    "more details.",
+    "",
+    "GoAccess is an open source and real-time Apache",
+    "web log analyzer that provides fast and valuable",
+    "HTTP statistics for system administrators that ",
+    "require a visual and interactive report on the fly.",
+    "",
+    "The data collected based on the parsing of the log",
+    "is divided into different modules. Modules are",
+    "automatically generated and presented to the user.",
+    "",
+    "The main window displays general statistics, top ",
+    "visitors, requests, browsers, operating systems,",
+    "hosts, etc.",
+    "",
+    "The user can make use of the following keys:",
+    " ^F1^ or ^CTRL^ + ^h^ [main help]",
+    " ^F5^    redraw [main window]",
+    " ^q^     quit the program/current window",
+    " ^o^     open detail view for selected module",
+    " ^RIGHT ARROW^ open detail view for selected",
+    "         module",
+    " ^TAB^   iterate modules (forward)",
+    " ^SHIFT^ + ^TAB^ iterate modules (backward)",
+    " ^0-9^   select module so the user can open a",
+    "         [detail view] with either ^o^ or",
+    "         ^RIGHT ARROW^",
+    " ^SHIFT^ + ^0-9^ select module above 10",
+    " ^s^     [detail view] sort unique visitors by date",
+    " ^S^     [detail view] sort unique visitors by hits",
+    " ^/^     [detail view] search forward for the ",
+    "         occurrence of pattern",
+    " ^n^     detail view - find the position of the next",
+    "         occurrence",
+    " ^t^     [detail view] move to the first item",
+    " ^b^     [detail view] move to the last item",
+    "",
+    "If you believe you have found a bug, please drop me",
+    "an email with details.",
+    "",
+    "If you have a medium or high traffic website, it",
+    "would be interesting to hear your experience with",
+    "GoAccess, such as generating time, visitors or hits.",
+    "",
+    "Send your questions, comments and suggestions to ",
+    "goaccess@prosoftcorp.com",
 };
 
-size_t os_size()
+void
+init_colors (void)
 {
-  return sizeof( os ) / sizeof( os[0] );
+    use_default_colors ();
+    init_pair (COL_BLUE, COLOR_BLUE, -1);
+    init_pair (COL_GREEN, COLOR_GREEN, -1);
+    init_pair (COL_RED, COLOR_RED, -1);
+    init_pair (COL_BLACK, COLOR_BLACK, -1);
+    init_pair (COL_CYAN, COLOR_CYAN, -1);
+    init_pair (COL_YELLOW, COLOR_YELLOW, -1);
+    init_pair (BLUE_GREEN, COLOR_BLUE, COLOR_GREEN);
+    init_pair (BLACK_GREEN, COLOR_BLACK, COLOR_GREEN);
+    init_pair (BLACK_CYAN, COLOR_BLACK, COLOR_CYAN);
+    init_pair (WHITE_RED, COLOR_WHITE, COLOR_RED);
 }
 
-size_t browsers_size()
+size_t
+os_size ()
 {
-  return sizeof( browsers ) / sizeof( browsers[0] );
+    return sizeof (os) / sizeof (os[0]);
 }
 
-size_t codes_size()
+size_t
+browsers_size ()
 {
-  return sizeof( codes ) / sizeof( codes[0] );
+    return sizeof (browsers) / sizeof (browsers[0]);
 }
 
-size_t help_main_size()
+size_t
+codes_size ()
 {
-  return sizeof( help_main ) / sizeof( help_main[0] );
+    return sizeof (codes) / sizeof (codes[0]);
+}
+
+size_t
+help_main_size ()
+{
+    return sizeof (help_main) / sizeof (help_main[0]);
 }
--- commons.h
+++ commons.h
@@ -2,7 +2,7 @@
  * Copyright (C) 2010 by Gerardo Orellana <goaccess@prosoftcorp.com>
  * GoAccess - An ncurses apache weblog analyzer & interactive viewer
  * @version 0.2
- * Last Modified: Saturday, July 10, 2010
+ * Last Modified: Sunday, July 25, 2010
  * Path:  /commons.h
  *
  * This program is distributed in the hope that it will be useful,
@@ -18,6 +18,7 @@
  */
 
 #include <glib.h>
+#include <curses.h>
 
 #ifndef COMMONS_H_INCLUDED
 #define COMMONS_H_INCLUDED
@@ -26,44 +27,53 @@
 #if !__GNUC__
 # define __attribute__(x) /**/
 #endif
-
 #define GO_UNUSED __attribute__((unused))
-
 #define DEBUG
 #define BUFFER 			4096
 #define MAX_CHOICES 	100
-#define TOTAL_MODULES 	10
+#define TOTAL_MODULES 	11
 #define GO_VERSION 		"0.2"
-
 #define MIN_HEIGHT 		40
 #define MIN_WIDTH       97
-
 /* max height of header window (rows) */
 #define MAX_HEIGHT_HEADER 6
 /* max height of footer stdscr (rows) */
 #define MAX_HEIGHT_FOOTER 1
-
 #define KB (1024)
 #define MB (KB * 1024)
 #define GB (MB * 1024)
+#define COL_WHITE    0
+#define COL_BLUE     1
+#define COL_GREEN    11
+#define COL_RED      3
+#define COL_BLACK    4
+#define COL_CYAN     5
+#define COL_YELLOW   6
+#define BLUE_GREEN   7
+#define BLACK_GREEN  8
+#define BLACK_CYAN   9
+#define WHITE_RED    10
 
-enum MODULES {
-	UNIQUE_VISITORS = 1,
-	REQUESTS,
-	REQUESTS_STATIC,
-	REFERRERS,
-	NOT_FOUND,
-	OS,
-	BROWSERS,
-	HOSTS,
-	STATUS_CODES,
-	REFERRING_SITES
+enum MODULES
+{
+    UNIQUE_VISITORS = 1,
+    REQUESTS,
+    REQUESTS_STATIC,
+    REFERRERS,
+    NOT_FOUND,
+    OS,
+    BROWSERS,
+    HOSTS,
+    STATUS_CODES,
+    REFERRING_SITES,
+    KEYPHRASES
 };
+
 extern enum MODULES modules;
 
 /* to create a new hash value out of a key, which can be NULL */
 extern GHashTable *ht_unique_visitors;
-extern GHashTable *ht_requests; 
+extern GHashTable *ht_requests;
 extern GHashTable *ht_referers;
 extern GHashTable *ht_unique_vis;
 extern GHashTable *ht_requests_static;
@@ -73,51 +83,58 @@ extern GHashTable *ht_browsers;
 extern GHashTable *ht_hosts;
 extern GHashTable *ht_status_code;
 extern GHashTable *ht_referring_sites;
+extern GHashTable *ht_keyphrases;
 
-struct logger {
-	char *host;
-	char *identd;
-	char *userid;
-	char *hour;
-	char *date;
-	char *request;
-	char *status;
-	char *referer;
-	char *agent;
-	int total_process;
-	int total_invalid;
-	int counter;
-	int alloc_counter;
-	int current_module;
-	int max_value;
+struct logger
+{
+    char *host;
+    char *identd;
+    char *userid;
+    char *hour;
+    char *date;
+    char *request;
+    char *status;
+    char *referer;
+    char *agent;
+    int total_process;
+    int total_invalid;
+    int counter;
+    int alloc_counter;
+    int current_module;
+    int max_value;
 };
+
 /*extern struct logger logger;  */
 
-struct stu_alloc_all {
-	char *data;
-	int  hits;
-	int module;
+struct struct_display
+{
+    char *data;
+    int hits;
+    int module;
 };
 
-struct stu_alloc_holder {
-	char *data;
-	int  hits;
+struct struct_holder
+{
+    char *data;
+    int hits;
 };
 
-struct scrolling {
-	size_t scrl_main_win;
-	size_t scrl_help_win;
-	size_t init_scrl_main_win;
+struct scrolling
+{
+    size_t scrl_main_win;
+    size_t scrl_help_win;
+    size_t init_scrl_main_win;
 };
 
 struct tm *now_tm;
+
 /*extern struct tm tm;*/
 
 /* Declaration of variables */
 extern int stripped_flag;
-extern int 	bandwidth_flag;
-extern int 	ignore_flag;
-extern int 	http_status_code_flag;
+extern int bandwidth_flag;
+extern int ignore_flag;
+extern int http_status_code_flag;
 extern long long req_size;
 extern char *req;
 extern char *stripped_str;
@@ -140,9 +157,10 @@ extern const char *os[];
 extern const char *browsers[];
 extern char *help_main[];
 
-size_t os_size(void);
-size_t browsers_size(void);
-size_t codes_size(void);
-size_t help_main_size(void);
+void init_colors (void);
+size_t os_size (void);
+size_t browsers_size (void);
+size_t codes_size (void);
+size_t help_main_size (void);
 
 #endif
