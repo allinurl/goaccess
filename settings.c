61bc06d9f49a206d04443aaf278170e5952b3b42
Gerardo Orellana <goaccess@prosoftcorp.com>
2010-09-05 10:16:51 -0500

    Update License

--- settings.c
+++ settings.c
@@ -1,3 +1,24 @@
+/**
+ * settings.c -- goaccess configuration
+ * Copyright (C) 2010 by Gerardo Orellana <goaccess@prosoftcorp.com>
+ * GoAccess - An ncurses apache weblog analyzer & interactive viewer
+ *
+ * This program is free software; you can redistribute it and/or    
+ * modify it under the terms of the GNU General Public License as   
+ * published by the Free Software Foundation; either version 2 of   
+ * the License, or (at your option) any later version.              
+ *                                                                  
+ * This program is distributed in the hope that it will be useful,  
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of   
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the    
+ * GNU General Public License for more details.                     
+ *                                                                  
+ * A copy of the GNU General Public License is attached to this 
+ * source distribution for its full text.
+ *
+ * Visit http://goaccess.prosoftcorp.com for new releases.
+ */
+
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
