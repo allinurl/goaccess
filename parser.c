a7f547f67912a8c457d1d0bf062db1bc5a108973
abgit <francisco.j.azevedo@gmail.com>
2013-09-05 17:24:55 +0100

    support for country & continent 2isocode prefix

--- parser.c
+++ parser.c
@@ -384,21 +384,21 @@ static void
 process_continent (const char *continentid)
 {
    if (memcmp (continentid, "NA", 2) == 0)
-      process_generic_data (ht_continents, "North America");
+      process_generic_data (ht_continents, "NA North America");
    else if (memcmp (continentid, "OC", 2) == 0)
-      process_generic_data (ht_continents, "Oceania");
+      process_generic_data (ht_continents, "OC Oceania");
    else if (memcmp (continentid, "EU", 2) == 0)
-      process_generic_data (ht_continents, "Europe");
+      process_generic_data (ht_continents, "EU Europe");
    else if (memcmp (continentid, "SA", 2) == 0)
-      process_generic_data (ht_continents, "South America");
+      process_generic_data (ht_continents, "SA South America");
    else if (memcmp (continentid, "AF", 2) == 0)
-      process_generic_data (ht_continents, "Africa");
+      process_generic_data (ht_continents, "AF Africa");
    else if (memcmp (continentid, "AN", 2) == 0)
-      process_generic_data (ht_continents, "Antarctica");
+      process_generic_data (ht_continents, "AN Antarctica");
    else if (memcmp (continentid, "AS", 2) == 0)
-      process_generic_data (ht_continents, "Asia");
+      process_generic_data (ht_continents, "AS Asia");
    else
-      process_generic_data (ht_continents, "Location Unknown");
+      process_generic_data (ht_continents, "-- Location Unknown");
 }
 #endif
 
@@ -816,7 +816,8 @@ process_log (GLog * logger, char *line, int test)
    int geo_id = GeoIP_id_by_name (geo_location_data, log->host);
 
    /* process country */
-   const char *location = get_geoip_data (log->host);
+   char location[30];
+   sprintf( location, "%s %s", GeoIP_code_by_id( geo_id ), get_geoip_data (log->host) );
    process_generic_data (ht_countries, location);
 
    /* process continent */
