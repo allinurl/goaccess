e6914e9d642a60c82d90fc91a9b97a7e4127acfc
Gerardo Orellana <goaccess@prosoftcorp.com>
2013-11-22 20:43:41 -0600

    Fixed pointer offset on static request extension.

--- parser.c
+++ parser.c
@@ -491,7 +491,7 @@ verify_static_content (char *req)
        !memcmp (nul - 4, ".css", 4) || !memcmp (nul - 4, ".CSS", 4) ||
        !memcmp (nul - 4, ".ico", 4) || !memcmp (nul - 4, ".ICO", 4) ||
        !memcmp (nul - 4, ".swf", 4) || !memcmp (nul - 4, ".SWF", 4) ||
-       !memcmp (nul - 5, ".jpeg", 5) || !memcmp (nul - 4, ".JPEG", 5))
+       !memcmp (nul - 5, ".jpeg", 5) || !memcmp (nul - 5, ".JPEG", 5))
       return 1;
    return 0;
 }
