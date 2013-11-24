fb42ecbdf2affcf7c92b8587d5534d1be3a9220d
Gerardo Orellana <goaccess@prosoftcorp.com>
2013-11-24 10:44:52 -0600

    Added JSON and CSV source and header files.

---
+++ json.h
@@ -0,0 +1,34 @@
+/**
+ * Copyright (C) 2009-2013 by Gerardo Orellana <goaccess@prosoftcorp.com>
+ * GoAccess - An Ncurses apache weblog analyzer & interactive viewer
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * A copy of the GNU General Public License is attached to this
+ * source distribution for its full text.
+ *
+ * Visit http://goaccess.prosoftcorp.com for new releases.
+ */
+
+#if HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#ifndef JSON_H_INCLUDED
+#define JSON_H_INCLUDED
+
+#include "gdashboard.h"
+#include "parser.h"
+#include "settings.h"
+
+void output_json (GLog * logger, GHolder * holder);
+
+#endif
