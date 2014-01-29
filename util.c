4f3f8aa23092748e9953a41432e3a0514c37657c
Gerardo Orellana <goaccess@prosoftcorp.com>
2014-01-28 21:33:54 -0600

    Replaced stderr with LOG_DEBUG when determining file size.

--- util.c
+++ util.c
@@ -694,8 +694,7 @@ file_size (const char *filename)
    if (stat (filename, &st) == 0)
       return st.st_size;
 
-   fprintf (stderr, "Cannot determine size of %s: %s\n", filename,
-            strerror (errno));
+   LOG_DEBUG (("Can't determine size of %s: %s\n", filename, strerror (errno)));
 
    return -1;
 }
