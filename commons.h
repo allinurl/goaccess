5dea66b6a8b9e67e6f37ffaccef8d2719f70f483
Gerardo Orellana <goaccess@prosoftcorp.com>
2014-04-13 17:15:38 -0500

    Version bumped to 0.8.

--- commons.h
+++ commons.h
@@ -36,7 +36,7 @@
 #define __attribute__(x) /**/
 #endif
 #define GO_UNUSED __attribute__((unused))
-#define GO_VERSION 		"0.7.1"
+#define GO_VERSION 		"0.8"
 #define GO_WEBSITE 		"http://goaccess.prosoftcorp.com/"
 struct tm *now_tm;
 
