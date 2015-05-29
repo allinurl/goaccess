ec9a8af1afa17a380796b62c06521975d9762645
Gerardo Orellana <goaccess@prosoftcorp.com>
2015-05-29 09:32:38 -0500

    Removed unnecessary includes from tokyo cabinet files.

--- src/tcabdb.c
+++ src/tcabdb.c
@@ -34,14 +34,8 @@
 #include "geolocation.h"
 #endif
 
-#include "browsers.h"
-#include "commons.h"
 #include "error.h"
-#include "opesys.h"
-#include "parser.h"
-#include "settings.h"
 #include "sort.h"
-#include "util.h"
 #include "xmalloc.h"
 
 GStorage *ht_storage;
