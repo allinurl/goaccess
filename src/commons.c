02852093ac09eded9ed68ce1e886fd6934ba01db
Gerardo Orellana <goaccess@prosoftcorp.com>
2015-06-21 09:34:06 -0500

    Removed unnecessary include from commons.c.

--- src/commons.c
+++ src/commons.c
@@ -23,16 +23,6 @@
 #include <config.h>
 #endif
 
-#ifdef HAVE_NCURSESW_NCURSES_H
-#include <ncursesw/ncurses.h>
-#elif HAVE_NCURSES_NCURSES_H
-#include <ncurses/ncurses.h>
-#elif HAVE_NCURSES_H
-#include <ncurses.h>
-#elif HAVE_CURSES_H
-#include <curses.h>
-#endif
-
 #include <string.h>
 #include <stdlib.h>
 #include <time.h>
