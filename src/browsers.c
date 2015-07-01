ea874d5eae8befeee0bd6ca97391d8aa17473074
Gerardo Orellana <goaccess@prosoftcorp.com>
2015-07-01 18:08:05 -0500

    Added 'Ruby' to the list of browsers.

--- src/browsers.c
+++ src/browsers.c
@@ -221,6 +221,7 @@ static const char *browsers[][2] = {
   {"Embedly", "Crawlers"},
   {"envolk", "Crawlers"},
   {"libwww-perl", "Crawlers"},
+  {"Ruby", "Crawlers"},
   {"python", "Crawlers"},
   {"Python", "Crawlers"},
   {"LinkedIn", "Crawlers"},
