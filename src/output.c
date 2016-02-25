28bd4ab76af21b40ce19869b466bc1db1e891d0e
Gerardo Orellana <goaccess@prosoftcorp.com>
2016-02-24 21:43:57 -0600

    Improved code readability on print_panel_def_meta().

--- src/output.c
+++ src/output.c
@@ -814,23 +814,24 @@ print_def_meta (FILE * fp, const char *head, const char *desc, int sp)
 static void
 print_panel_def_meta (FILE * fp, const GHTML * def, int sp)
 {
-  int isp = 0;
+  const char *head = module_to_head (def->module);
+  const char *desc = module_to_desc (def->module);
+  const char *id = module_to_id (def->module);
+  const char *chart = chart2str (def->chart_type);
+
+  int isp = 0, reverse = def->chart_reverse;;
   /* use tabs to prettify output */
   if (conf.json_pretty_print)
     isp = sp + 1;
 
-  print_def_meta (fp, module_to_head (def->module),
-                  module_to_desc (def->module), sp);
+  print_def_meta (fp, head, desc, sp);
 
-  pjson (fp, "%.*s'id': '%s',%.*s", isp, TAB, module_to_id (def->module),
-         nlines, NL);
+  pjson (fp, "%.*s'id': '%s',%.*s", isp, TAB, id, nlines, NL);
   pjson (fp, "%.*s'table': %d,%.*s", isp, TAB, def->table, nlines, NL);
 
   if (def->chart_type) {
-    pjson (fp, "%.*s'chartType': '%s',%.*s", isp, TAB,
-           chart2str (def->chart_type), nlines, NL);
-    pjson (fp, "%.*s'chartReverse': %d,%.*s", isp, TAB, def->chart_reverse,
-           nlines, NL);
+    pjson (fp, "%.*s'chartType': '%s',%.*s", isp, TAB, chart, nlines, NL);
+    pjson (fp, "%.*s'chartReverse': %d,%.*s", isp, TAB, reverse, nlines, NL);
   }
 
   print_def_plot (fp, def, isp);
