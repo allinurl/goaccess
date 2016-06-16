06682385081c9454057649d122251e17f45700a2
Gerardo Orellana <goaccess@prosoftcorp.com>
2016-06-16 07:16:34 -0500

    Display Android version number along with the codename, e.g., 'Lollipop 5.1'.

--- src/opesys.c
+++ src/opesys.c
@@ -120,28 +120,33 @@ static char *
 get_real_android (const char *droid)
 {
   if (strstr (droid, "6.0"))
-    return alloc_string ("Marshmallow");
-  else if (strstr (droid, "5.0") || strstr (droid, "5.1"))
-    return alloc_string ("Lollipop");
+    return alloc_string ("Marshmallow 6.0");
+  else if (strstr (droid, "5.1"))
+    return alloc_string ("Lollipop 5.1");
+  else if (strstr (droid, "5.0"))
+    return alloc_string ("Lollipop 5.0");
   else if (strstr (droid, "4.4"))
-    return alloc_string ("KitKat");
-  else if (strstr (droid, "4.3") || strstr (droid, "4.2") ||
-           strstr (droid, "4.1"))
-    return alloc_string ("Jelly Bean");
+    return alloc_string ("KitKat 4.4");
+  else if (strstr (droid, "4.3"))
+    return alloc_string ("Jelly Bean 4.3");
+  else if (strstr (droid, "4.2"))
+    return alloc_string ("Jelly Bean 4.2");
+  else if (strstr (droid, "4.1"))
+    return alloc_string ("Jelly Bean 4.1");
   else if (strstr (droid, "4.0"))
-    return alloc_string ("Ice Cream Sandwich");
+    return alloc_string ("Ice Cream Sandwich 4.0");
   else if (strstr (droid, "3."))
-    return alloc_string ("Honeycomb");
+    return alloc_string ("Honeycomb 3");
   else if (strstr (droid, "2.3"))
-    return alloc_string ("Gingerbread");
+    return alloc_string ("Gingerbread 2.3");
   else if (strstr (droid, "2.2"))
-    return alloc_string ("Froyo");
+    return alloc_string ("Froyo 2.2");
   else if (strstr (droid, "2.0") || strstr (droid, "2.1"))
-    return alloc_string ("Eclair");
+    return alloc_string ("Eclair 2");
   else if (strstr (droid, "1.6"))
-    return alloc_string ("Donut");
+    return alloc_string ("Donut 1.6");
   else if (strstr (droid, "1.5"))
-    return alloc_string ("Cupcake");
+    return alloc_string ("Cupcake 1.5");
   return alloc_string (droid);
 }
 
