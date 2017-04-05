e2f390558f294b4120e547170f39ab87177c1167
Gerardo Orellana <goaccess@prosoftcorp.com>
2017-04-04 19:12:05 -0500

    Moved string literals to their own labels header file.

--- src/ui.h
+++ src/ui.h
@@ -26,10 +26,6 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
-#include <libintl.h>
-#define _(String) dgettext (PACKAGE , String)
-#define gettext_noop(String) String
-#define N_(String) gettext_noop (String)
 
 #ifndef UI_H_INCLUDED
 #define UI_H_INCLUDED
@@ -48,6 +44,9 @@
 #include <pthread.h>
 #endif
 
+/* string literals and translations */
+#include "labels.h"
+
 /* Global UI defaults */
 #define MIN_HEIGHT        8     /* minimum window height */
 #define MIN_WIDTH         0     /* minimum window width */
@@ -55,109 +54,28 @@
 #define MAX_HEIGHT_HEADER 7     /* height of the header window */
 #define OVERALL_NUM_COLS  4     /* number of columns on the overall stats win */
 
-/* Overall Stats Labels */
-#define T_DASH       _("Dashboard")
-#define T_HEAD       _("Overall Analyzed Requests")
-#define T_DASH_HEAD  _("Dashboard - Overall Analyzed Requests")
-
-#define T_DATETIME   _("Date/Time")
-#define T_REQUESTS   _("Total Requests")
-#define T_GEN_TIME   _("Init. Proc. Time")
-#define T_FAILED     _("Failed Requests")
-#define T_VALID      _("Valid Requests")
-#define T_UNIQUE_VIS _("Unique Visitors")
-#define T_UNIQUE_FIL _("Unique Files")
-#define T_EXCLUDE_IP _("Excl. IP Hits")
-#define T_REFERRER   _("Referrers")
-#define T_UNIQUE404  _("Unique 404")
-#define T_STATIC_FIL _("Static Files")
-#define T_LOG        _("Log Size")
-#define T_BW         _("Bandwidth")
-#define T_LOG_PATH   _("Log Source")
-
 /* Spinner Label Format */
 #define SPIN_FMT "%s"
 #define SPIN_FMTM "%s [%'d] [%'lld/s]"
 #define SPIN_LBL 50     /* max length of the progress spinner */
 
-/* Module Labels and Descriptions */
-#define VISIT_HEAD  N_("Unique visitors per day")
-#define VISIT_HBOTS N_("Unique visitors per day - Including spiders")
-#define VISIT_DESC  N_("Hits having the same IP, date and agent are a unique visit.")
-#define VISIT_LABEL N_("Visitors")
-#define VISIT_ID    "visitors"
-
-#define REQUE_HEAD  N_("Requested Files (URLs)")
-#define REQUE_DESC  N_("Top requests sorted by hits [, avgts, cumts, maxts, mthd, proto]")
-#define REQUE_LABEL N_("Requests")
-#define REQUE_ID    "requests"
-
-#define STATI_HEAD  N_("Static Requests")
-#define STATI_DESC  N_("Top static requests sorted by hits [, avgts, cumts, maxts, mthd, proto]")
-#define STATI_LABEL N_("Static Requests")
-#define STATI_ID    "static_requests"
-
-#define VTIME_HEAD  N_("Time Distribution")
-#define VTIME_DESC  N_("Data sorted by hour [, avgts, cumts, maxts]")
-#define VTIME_LABEL N_("Time")
-#define VTIME_ID    "visit_time"
-
-#define VHOST_HEAD  N_("Virtual Hosts")
-#define VHOST_DESC  N_("Data sorted by hits [, avgts, cumts, maxts]")
-#define VHOST_LABEL N_("Virtual Hosts")
-#define VHOST_ID    "vhosts"
-
-#define RUSER_HEAD  N_("Remote User (HTTP authentication)")
-#define RUSER_DESC  N_("Data sorted by hits [, avgts, cumts, maxts]")
-#define RUSER_LABEL N_("Remote User")
-#define RUSER_ID    "remote_user"
-
-#define FOUND_HEAD  N_("Not Found URLs (404s)")
-#define FOUND_DESC  N_("Top not found URLs sorted by hits [, avgts, cumts, maxts, mthd, proto]")
-#define FOUND_LABEL N_("Not Found")
-#define FOUND_ID    "not_found"
-
-#define HOSTS_HEAD  N_("Visitor Hostnames and IPs")
-#define HOSTS_DESC  N_("Top visitor hosts sorted by hits [, avgts, cumts, maxts]")
-#define HOSTS_LABEL N_("Hosts")
-#define HOSTS_ID    "hosts"
-
-#define OPERA_HEAD  N_("Operating Systems")
-#define OPERA_DESC  N_("Top Operating Systems sorted by hits [, avgts, cumts, maxts]")
-#define OPERA_LABEL N_("OS")
-#define OPERA_ID    "os"
-
-#define BROWS_HEAD  N_("Browsers")
-#define BROWS_DESC  N_("Top Browsers sorted by hits [, avgts, cumts, maxts]")
-#define BROWS_LABEL N_("Browsers")
-#define BROWS_ID    "browsers"
-
-#define REFER_HEAD  N_("Referrers URLs")
-#define REFER_DESC  N_("Top Requested Referrers sorted by hits [, avgts, cumts, maxts]")
-#define REFER_LABEL N_("Referrers")
-#define REFER_ID    "referrers"
-
-#define SITES_HEAD  N_("Referring Sites")
-#define SITES_DESC  N_("Top Referring Sites sorted by hits [, avgts, cumts, maxts]")
-#define SITES_LABEL N_("Referring Sites")
-#define SITES_ID    "referring_sites"
-
-#define KEYPH_HEAD  N_("Keyphrases from Google's search engine")
-#define KEYPH_DESC  N_("Top Keyphrases sorted by hits [, avgts, cumts, maxts]")
-#define KEYPH_LABEL N_("Keyphrases")
-#define KEYPH_ID    "keyphrases"
-
-#define GEOLO_HEAD  N_("Geo Location")
-#define GEOLO_DESC  N_("Continent > Country sorted by unique hits [, avgts, cumts, maxts]")
-#define GEOLO_LABEL N_("Geo Location")
-#define GEOLO_ID    "geolocation"
-
-#define CODES_HEAD  N_("HTTP Status Codes")
-#define CODES_DESC  N_("Top HTTP Status Codes sorted by hits [, avgts, cumts, maxts]")
-#define CODES_LABEL N_("Status Codes")
-#define CODES_ID    "status_codes"
-
-#define GENER_ID   "general"
+/* Module JSON keys */
+#define VISIT_ID "visitors"
+#define REQUE_ID "requests"
+#define STATI_ID "static_requests"
+#define VTIME_ID "visit_time"
+#define VHOST_ID "vhosts"
+#define RUSER_ID "remote_user"
+#define FOUND_ID "not_found"
+#define HOSTS_ID "hosts"
+#define OPERA_ID "os"
+#define BROWS_ID "browsers"
+#define REFER_ID "referrers"
+#define SITES_ID "referring_sites"
+#define KEYPH_ID "keyphrases"
+#define GEOLO_ID "geolocation"
+#define CODES_ID "status_codes"
+#define GENER_ID "general"
 
 /* Overall Statistics CSV/JSON Keys */
 #define OVERALL_STARTDATE "start_date"
@@ -176,27 +94,6 @@
 #define OVERALL_LOGSIZE   "log_size"
 #define OVERALL_BANDWIDTH "bandwidth"
 #define OVERALL_LOG       "log_path"
-
-/* Metric Labels */
-#define MTRC_HITS_LBL            _("Hits")
-#define MTRC_VISITORS_LBL        _("Visitors")
-#define MTRC_VISITORS_SHORT_LBL  _("Vis.")
-#define MTRC_BW_LBL              _("Bandwidth")
-#define MTRC_AVGTS_LBL           _("Avg. T.S.")
-#define MTRC_CUMTS_LBL           _("Cum. T.S.")
-#define MTRC_MAXTS_LBL           _("Max. T.S.")
-#define MTRC_METHODS_LBL         _("Method")
-#define MTRC_METHODS_SHORT_LBL   _("Mtd")
-#define MTRC_PROTOCOLS_LBL       _("Protocol")
-#define MTRC_PROTOCOLS_SHORT_LBL _("Proto")
-#define MTRC_CITY_LBL            _("City")
-#define MTRC_COUNTRY_LBL         _("Country")
-#define MTRC_HOSTNAME_LBL        _("Hostname")
-#define MTRC_DATA_LBL            _("Data")
-
-/* Find Labels */
-#define FIND_HEAD    _("Find pattern in all views")
-#define FIND_DESC    _("Regex allowed - ^g to cancel - TAB switch case")
 
 /* CONFIG DIALOG */
 #define CONF_MENU_H       6
@@ -226,8 +123,6 @@
 #define SORT_MENU_Y       4
 #define SORT_WIN_H        13
 #define SORT_WIN_W        42
-#define SORT_ASC_SEL      "[x] ASC [ ] DESC"
-#define SORT_DESC_SEL     "[ ] ASC [x] DESC"
 
 /* AGENTS DIALOG */
 #define AGENTS_MENU_X     2
@@ -242,16 +137,12 @@
 #define HELP_WIN_WIDTH    64
 
 /* CONF ERROR DIALOG */
-#define ERR_MENU_HEIGHT  10
-#define ERR_MENU_WIDTH   57
-#define ERR_MENU_X       2
-#define ERR_MENU_Y       4
-#define ERR_WIN_HEIGHT   15
-#define ERR_WIN_WIDTH    61
-#define ERR_HEADER _("Format Errors - Verify your log/date/time format")
-
-#define CSENSITIVE   _("[x] case sensitive")
-#define CISENSITIVE  _("[ ] case sensitive")
+#define ERR_MENU_HEIGHT   10
+#define ERR_MENU_WIDTH    57
+#define ERR_MENU_X        2
+#define ERR_MENU_Y        4
+#define ERR_WIN_HEIGHT    15
+#define ERR_WIN_WIDTH     61
 
 /* Convenient macros */
 #define MIN(a, b) (((a) < (b)) ? (a) : (b))
