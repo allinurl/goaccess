c6b9bb11776f1101d08c436f4be64c78b14cb4aa
fqbuild <fqbuild@FutureQuest.net>
2017-10-25 03:57:24 -0700

    parser.c: Fixes another travis-ci build problem where fgetline() should only be built if WITH_GETLINE is in play.
      fgetline() is only called from a wrapped
      >> #ifdef WITH_GETLINE
      >> read_lines() {
      >> ==> fgetline()
      >> }
      >> #endif

--- src/parser.c
+++ src/parser.c
@@ -2580,6 +2580,7 @@ read_line (GLog * glog, char *line, int *test, int *cnt, int dry_run)
  *
  * On error, NULL is returned.
  * On success, the malloc'd line is returned. */
+#ifdef WITH_GETLINE
 char *
 fgetline (FILE * fp)
 {
@@ -2629,6 +2630,7 @@ fgetline (FILE * fp)
 
   return NULL;
 }
+#endif
 
 /* Iterate over the log and read line by line (use GNU get_line to parse the
  * whole line).
