5f1562b1addf6d5692a79e4e1c5937793d755434
Gerardo Orellana <goaccess@prosoftcorp.com>
2018-02-14 20:15:29 -0600

    Fixed JS jshint warning 'host is already defined'.

--- resources/js/app.js
+++ resources/js/app.js
@@ -80,7 +80,8 @@ window.GoAccess = window.GoAccess || {
 	},
 
 	setWebSocket: function (wsConn) {
-		var host = null, host = wsConn.url ? wsConn.url : window.location.hostname ? window.location.hostname : "localhost";
+		var host = null;
+		host = wsConn.url ? wsConn.url : window.location.hostname ? window.location.hostname : "localhost";
 		var str = /^(wss?:\/\/)?[^\/]+:[0-9]{1,5}\//.test(host + "/") ? host : String(host + ':' + wsConn.port);
 		str = !/^wss?:\/\//i.test(str) ? (window.location.protocol === "https:" ? 'wss://' : 'ws://') + str : str;
 
