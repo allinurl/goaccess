a9bf88c0904d2f1e0a6dfe16dff1461ebf88443d
Gerardo O <goaccess@prosoftcorp.com>
2019-11-28 14:07:34 -0600

    Added Android 10 to the list of Android codenames.

--- src/opesys.c
+++ src/opesys.c
@@ -121,7 +121,9 @@ static const char *os[][2] = {
 static char *
 get_real_android (const char *droid)
 {
-  if (strstr (droid, "9"))
+  if (strstr (droid, "10"))
+    return alloc_string ("Android 10");
+  else if (strstr (droid, "9"))
     return alloc_string ("Pie 9");
   else if (strstr (droid, "8.1"))
     return alloc_string ("Oreo 8.1");
