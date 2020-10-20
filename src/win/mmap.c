f4ebc33b180a0c00c370156190d227476a8fed7e
Gerardo O <goaccess@prosoftcorp.com>
2020-10-20 17:46:41 -0500

    Updated formatting + indentation.

--- src/win/mmap.c
+++ src/win/mmap.c
@@ -6,7 +6,7 @@
 #include <errno.h>
 #include "mman.h"
 
-static const char id[]="$Id: tpl.c 107 2007-04-20 17:11:29Z thanson $";
+static const char id[] = "$Id: tpl.c 107 2007-04-20 17:11:29Z thanson $";
 
 /**
  * @brief Map a file to a memory region
@@ -22,96 +22,96 @@ static const char id[]="$Id: tpl.c 107 2007-04-20 17:11:29Z thanson $";
  * @param offset offset into mapped object
  * @return pointer to the memory region, or NULL in case of error
  */
-void *mmap(void *addr, unsigned int len, int prot, int flags, int fd, unsigned int offset)
-{
-	DWORD wprot;
-	DWORD waccess;
-	HANDLE h;
-	void *region;
+void *
+mmap (void *addr, unsigned int len, int prot, int flags, int fd, unsigned int offset) {
+  DWORD wprot;
+  DWORD waccess;
+  HANDLE h;
+  void *region;
 
-	/* Translate read/write/exec flags into WIN32 constants */
-	switch (prot) {
-	case PROT_READ:
-		wprot = PAGE_READONLY;
-		break;
-	case PROT_EXEC:
-		wprot = PAGE_EXECUTE_READ;
-		break;
-	case PROT_READ | PROT_EXEC:
-		wprot = PAGE_EXECUTE_READ;
-		break;
-	case PROT_WRITE:
-		wprot = PAGE_READWRITE;
-		break;
-	case PROT_READ | PROT_WRITE:
-		wprot = PAGE_READWRITE;
-		break;
-	case PROT_READ | PROT_WRITE | PROT_EXEC:
-		wprot = PAGE_EXECUTE_READWRITE;
-		break;
-	case PROT_WRITE | PROT_EXEC:
-		wprot = PAGE_EXECUTE_READWRITE;
-		break;
-	}
-	
-	/* Obtaing handle to map region */
-	h = CreateFileMapping((HANDLE) _get_osfhandle(fd), 0, wprot, 0, len, 0);
-	if (h == NULL) {
-		DWORD error = GetLastError();
+  /* Translate read/write/exec flags into WIN32 constants */
+  switch (prot) {
+  case PROT_READ:
+    wprot = PAGE_READONLY;
+    break;
+  case PROT_EXEC:
+    wprot = PAGE_EXECUTE_READ;
+    break;
+  case PROT_READ | PROT_EXEC:
+    wprot = PAGE_EXECUTE_READ;
+    break;
+  case PROT_WRITE:
+    wprot = PAGE_READWRITE;
+    break;
+  case PROT_READ | PROT_WRITE:
+    wprot = PAGE_READWRITE;
+    break;
+  case PROT_READ | PROT_WRITE | PROT_EXEC:
+    wprot = PAGE_EXECUTE_READWRITE;
+    break;
+  case PROT_WRITE | PROT_EXEC:
+    wprot = PAGE_EXECUTE_READWRITE;
+    break;
+  }
 
-		/* Try and translate some error codes */
-		switch (error) {
-		case ERROR_ACCESS_DENIED:
-		case ERROR_INVALID_ACCESS:
-			errno = EACCES;
-			break;
-		case ERROR_OUTOFMEMORY:
-		case ERROR_NOT_ENOUGH_MEMORY:
-			errno = ENOMEM;
-			break;
-		default:
-			errno = EINVAL;
-			break;
-		}
-		return MAP_FAILED;
-	}
-			
+  /* Obtaing handle to map region */
+  h = CreateFileMapping ((HANDLE) _get_osfhandle (fd), 0, wprot, 0, len, 0);
+  if (h == NULL) {
+    DWORD error = GetLastError ();
 
-	/* Translate sharing options into WIN32 constants */
-	switch (wprot) {
-	case PAGE_READONLY:
-		waccess = FILE_MAP_READ;
-		break;
-	case PAGE_READWRITE:
-		waccess = FILE_MAP_WRITE;
-		break;
-	}
+    /* Try and translate some error codes */
+    switch (error) {
+    case ERROR_ACCESS_DENIED:
+    case ERROR_INVALID_ACCESS:
+      errno = EACCES;
+      break;
+    case ERROR_OUTOFMEMORY:
+    case ERROR_NOT_ENOUGH_MEMORY:
+      errno = ENOMEM;
+      break;
+    default:
+      errno = EINVAL;
+      break;
+    }
+    return MAP_FAILED;
+  }
 
-	/* Map file and return pointer */
-	region = MapViewOfFile(h, waccess, 0, 0, 0);
-	if (region == NULL) {
-		DWORD error = GetLastError();
 
-		/* Try and translate some error codes */
-		switch (error) {
-		case ERROR_ACCESS_DENIED:
-		case ERROR_INVALID_ACCESS:
-			errno = EACCES;
-			break;
-		case ERROR_INVALID_HANDLE:
-			errno = EBADF;
-			break;
-		default:
-			errno = EINVAL;
-			break;
-		}
-		CloseHandle(h);
-		return MAP_FAILED;
-	}
-	CloseHandle(h); /* ok to call UnmapViewOfFile after this */
-	
-	/* All fine */
-	return region;
+  /* Translate sharing options into WIN32 constants */
+  switch (wprot) {
+  case PAGE_READONLY:
+    waccess = FILE_MAP_READ;
+    break;
+  case PAGE_READWRITE:
+    waccess = FILE_MAP_WRITE;
+    break;
+  }
+
+  /* Map file and return pointer */
+  region = MapViewOfFile (h, waccess, 0, 0, 0);
+  if (region == NULL) {
+    DWORD error = GetLastError ();
+
+    /* Try and translate some error codes */
+    switch (error) {
+    case ERROR_ACCESS_DENIED:
+    case ERROR_INVALID_ACCESS:
+      errno = EACCES;
+      break;
+    case ERROR_INVALID_HANDLE:
+      errno = EBADF;
+      break;
+    default:
+      errno = EINVAL;
+      break;
+    }
+    CloseHandle (h);
+    return MAP_FAILED;
+  }
+  CloseHandle (h);      /* ok to call UnmapViewOfFile after this */
+
+  /* All fine */
+  return region;
 }
 
 
@@ -119,20 +119,19 @@ void *mmap(void *addr, unsigned int len, int prot, int flags, int fd, unsigned i
  * @brief Unmap a memory region
  *
  * This is a wrapper around UnmapViewOfFile in the win32 API
- * 
+ *
  * @param addr start address
  * @param len length of the region
  * @return 0 for success, -1 for error
  */
-int munmap(void *addr, int len) 
-{
-	if (UnmapViewOfFile(addr)) {
-		return 0;
-	}
-	else {
-		errno = EINVAL;
-		return -1;
-	}
+int
+munmap (void *addr, int len) {
+  if (UnmapViewOfFile (addr)) {
+    return 0;
+  } else {
+    errno = EINVAL;
+    return -1;
+  }
 }
 
 
@@ -146,26 +145,26 @@ int munmap(void *addr, int len)
  * @param flags sync options -- currently ignored
  * @return 0 for success, -1 for error
  */
-int msync(char *addr, int len, int flags) 
-{
-	if (FlushViewOfFile(addr, len) == 0) {
-		DWORD error = GetLastError();
-		
-		/* Try and translate some error codes */
-		switch (error) {
-		case ERROR_INVALID_PARAMETER:
-			errno = EINVAL;
-			break;
-		case ERROR_WRITE_FAULT:
-			errno = EIO;
-			break;
-		default:
-			errno = EINVAL;
-			break;
-		}
-		return -1;
-	}
+int
+msync (char *addr, int len, int flags) {
+  if (FlushViewOfFile (addr, len) == 0) {
+    DWORD error = GetLastError ();
 
-	/* Success */
-	return 0;
+    /* Try and translate some error codes */
+    switch (error) {
+    case ERROR_INVALID_PARAMETER:
+      errno = EINVAL;
+      break;
+    case ERROR_WRITE_FAULT:
+      errno = EIO;
+      break;
+    default:
+      errno = EINVAL;
+      break;
+    }
+    return -1;
+  }
+
+  /* Success */
+  return 0;
 }
