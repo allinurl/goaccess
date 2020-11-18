41e27b8cb5b728e8c235fbc437d251a01e9fd28f
Gerardo O <goaccess@prosoftcorp.com>
2020-11-17 20:00:37 -0600

    Fixed typo when setting struct member if no connection has been set.

--- src/websocket.c
+++ src/websocket.c
@@ -1473,7 +1473,7 @@ ws_set_handshake_headers (WSHeaders * headers) {
   if (!headers->upgrade)
     headers->upgrade = xstrdup ("websocket");
   if (!headers->connection)
-    headers->upgrade = xstrdup ("Upgrade");
+    headers->connection = xstrdup ("Upgrade");
 
   free (s);
 }
