2ae93b0d58ae67f7ce5926397c377fcfbaadbda6
Gerardo O <goaccess@prosoftcorp.com>
2021-01-06 17:00:14 -0600

    Updated pre-defined Caddy log-format.

--- src/settings.c
+++ src/settings.c
@@ -79,7 +79,7 @@ static const GPreConfLog logs = {
   /* Caddy JSON */
   "{\"ts\":\"%x.%^\",\"request\":{\"remote_addr\":\"%h:%^\",\"proto\":\"%H\","
     "\"method\":\"%m\",\"host\":\"%v\",\"uri\":\"%U\","
-    "\"headers\":{\"User-Agent\":[\"%u\",\"%^\"]},\"tls\":{\"cipher_suite\":\"%k\","
+    "\"headers\":{\"User-Agent\":[\"%u\"]},\"tls\":{\"cipher_suite\":\"%k\","
     "\"proto\":\"%K\"}},\"duration\":\"%T\",\"size\":\"%b\",\"status\":\"%s\","
     "\"resp_headers\":{\"Content-Type\":[\"%M\"]}}"
 };
