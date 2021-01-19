allinurl/goaccess

commit d7318ac9122f91b68188a1795f56c4ecf1e911f1

Ensure TUI/CSV/HTML reports are able to output uint64_t data.
This also depends on the backend storage.

src/gkhash.c
@@ -2075,35 +2075,6 @@ ht_get_keymap_list_from_key (GModule module, const char *key) {
 
 /* Iterate over all the key/value pairs for the given hash structure
  * and set the maximum and minimum values found on an integer key and
- * integer value.
- *
- * Note: This are expensive calls since it has to iterate over all
- * key-value pairs
- *
- * If the hash structure is empty, no values are set.
- * On success the minimum and maximum values are set. */
-static void
-get_ii32_min_max (khash_t (ii32) * hash, uint32_t * min, uint32_t * max) {
-  khint_t k;
-  uint32_t curvalue = 0;
-  int i;
-
-  for (i = 0, k = kh_begin (hash); k != kh_end (hash); ++k) {
-    if (!kh_exist (hash, k))
-      continue;
-
-    curvalue = kh_value (hash, k);
-    if (i++ == 0)
-      *min = curvalue;
-    if (curvalue > *max)
-      *max = curvalue;
-    if (curvalue < *min)
-      *min = curvalue;
-  }
-}
-
-/* Iterate over all the key/value pairs for the given hash structure
- * and set the maximum and minimum values found on an integer key and
  * a uint64_t value.
  *
  * Note: This are expensive calls since it has to iterate over all
@@ -2998,13 +2969,13 @@ ht_get_meta_data (GModule module, const char *key) {
  * If the hash structure is empty, no values are set.
  * On success the minimum and maximum values are set. */
 void
-ht_get_hits_min_max (GModule module, uint32_t * min, uint32_t * max) {
-  khash_t (ii32) * cache = get_hash_from_cache (module, MTRC_HITS);
+ht_get_hits_min_max (GModule module, uint64_t * min, uint64_t * max) {
+  khash_t (iu64) * cache = get_hash_from_cache (module, MTRC_HITS);
 
   if (!cache)
     return;
 
-  get_ii32_min_max (cache, min, max);
+  get_iu64_min_max (cache, min, max);
 }
 
 /* Set the maximum and minimum values found on an integer key and
@@ -3013,13 +2984,13 @@ ht_get_hits_min_max (GModule module, uint32_t * min, uint32_t * max) {
  * If the hash structure is empty, no values are set.
  * On success the minimum and maximum values are set. */
 void
-ht_get_visitors_min_max (GModule module, uint32_t * min, uint32_t * max) {
-  khash_t (ii32) * cache = get_hash_from_cache (module, MTRC_VISITORS);
+ht_get_visitors_min_max (GModule module, uint64_t * min, uint64_t * max) {
+  khash_t (iu64) * cache = get_hash_from_cache (module, MTRC_VISITORS);
 
   if (!cache)
     return;
 
-  get_ii32_min_max (cache, min, max);
+  get_iu64_min_max (cache, min, max);
 }
 
 /* Set the maximum and minimum values found on an integer key and

src/gkhash.h
@@ -351,9 +351,9 @@ void destroy_date_stores (int date);
 void free_storage (void);
 void ht_get_bw_min_max (GModule module, uint64_t * min, uint64_t * max);
 void ht_get_cumts_min_max (GModule module, uint64_t * min, uint64_t * max);
-void ht_get_hits_min_max (GModule module, uint32_t * min, uint32_t * max);
+void ht_get_hits_min_max (GModule module, uint64_t * min, uint64_t * max);
 void ht_get_maxts_min_max (GModule module, uint64_t * min, uint64_t * max);
-void ht_get_visitors_min_max (GModule module, uint32_t * min, uint32_t * max);
+void ht_get_visitors_min_max (GModule module, uint64_t * min, uint64_t * max);
 void init_pre_storage (void);
 void init_storage (void);
 void u64decode (uint64_t n, uint32_t * x, uint32_t * y);

src/json.c
@@ -325,15 +325,6 @@ fpskeysval (FILE * fp, const char *key, const char *val, int sp, int last) {
     fpjson (fp, "%.*s\"%s\": \"%s\"", sp, TAB, key, val);
 }
 
-/* Write to a buffer a JSON string key, int value pair. */
-static void
-pskeyival (GJSON * json, const char *key, int val, int sp, int last) {
-  if (!last)
-    pjson (json, "%.*s\"%s\": %d,%.*s", sp, TAB, key, val, nlines, NL);
-  else
-    pjson (json, "%.*s\"%s\": %d", sp, TAB, key, val);
-}
-
 /* Output a JSON string key, int value pair. */
 void
 fpskeyival (FILE * fp, const char *key, int val, int sp, int last) {
@@ -468,21 +459,21 @@ poverall_start_end_date (GJSON * json, GHolder * h, int sp) {
 /* Write to a buffer date and time for the overall object. */
 static void
 poverall_requests (GJSON * json, int sp) {
-  pskeyival (json, OVERALL_REQ, ht_get_processed (), sp, 0);
+  pskeyu64val (json, OVERALL_REQ, ht_get_processed (), sp, 0);
 }
 
 /* Write to a buffer the number of valid requests under the overall
  * object. */
 static void
 poverall_valid_reqs (GJSON * json, int sp) {
-  pskeyival (json, OVERALL_VALID, ht_sum_valid (), sp, 0);
+  pskeyu64val (json, OVERALL_VALID, ht_sum_valid (), sp, 0);
 }
 
 /* Write to a buffer the number of invalid requests under the overall
  * object. */
 static void
 poverall_invalid_reqs (GJSON * json, int sp) {
-  pskeyival (json, OVERALL_FAILED, ht_get_invalid (), sp, 0);
+  pskeyu64val (json, OVERALL_FAILED, ht_get_invalid (), sp, 0);
 }
 
 /* Write to a buffer the total processed time under the overall
@@ -496,41 +487,41 @@ poverall_processed_time (GJSON * json, int sp) {
  * overall object. */
 static void
 poverall_visitors (GJSON * json, int sp) {
-  pskeyival (json, OVERALL_VISITORS, ht_get_size_uniqmap (VISITORS), sp, 0);
+  pskeyu64val (json, OVERALL_VISITORS, ht_get_size_uniqmap (VISITORS), sp, 0);
 }
 
 /* Write to a buffer the total number of unique files under the
  * overall object. */
 static void
 poverall_files (GJSON * json, int sp) {
-  pskeyival (json, OVERALL_FILES, ht_get_size_datamap (REQUESTS), sp, 0);
+  pskeyu64val (json, OVERALL_FILES, ht_get_size_datamap (REQUESTS), sp, 0);
 }
 
 /* Write to a buffer the total number of excluded requests under the
  * overall object. */
 static void
 poverall_excluded (GJSON * json, int sp) {
-  pskeyival (json, OVERALL_EXCL_HITS, ht_get_excluded_ips (), sp, 0);
+  pskeyu64val (json, OVERALL_EXCL_HITS, ht_get_excluded_ips (), sp, 0);
 }
 
 /* Write to a buffer the number of referrers under the overall object. */
 static void
 poverall_refs (GJSON * json, int sp) {
-  pskeyival (json, OVERALL_REF, ht_get_size_datamap (REFERRERS), sp, 0);
+  pskeyu64val (json, OVERALL_REF, ht_get_size_datamap (REFERRERS), sp, 0);
 }
 
 /* Write to a buffer the number of not found (404s) under the overall
  * object. */
 static void
 poverall_notfound (GJSON * json, int sp) {
-  pskeyival (json, OVERALL_NOTFOUND, ht_get_size_datamap (NOT_FOUND), sp, 0);
+  pskeyu64val (json, OVERALL_NOTFOUND, ht_get_size_datamap (NOT_FOUND), sp, 0);
 }
 
 /* Write to a buffer the number of static files (jpg, pdf, etc) under
  * the overall object. */
 static void
 poverall_static_files (GJSON * json, int sp) {
-  pskeyival (json, OVERALL_STATIC, ht_get_size_datamap (REQUESTS_STATIC), sp, 0);
+  pskeyu64val (json, OVERALL_STATIC, ht_get_size_datamap (REQUESTS_STATIC), sp, 0);
 }
 
 /* Write to a buffer the size of the log being parsed under the
@@ -585,7 +576,7 @@ phits (GJSON * json, GMetrics * nmetrics, int sp) {
 
   popen_obj_attr (json, "hits", sp);
   /* print hits */
-  pskeyival (json, "count", nmetrics->hits, isp, 0);
+  pskeyu64val (json, "count", nmetrics->hits, isp, 0);
   /* print hits percent */
   pskeyfval (json, "percent", nmetrics->hits_perc, isp, 1);
   pclose_obj (json, sp, 0);
@@ -602,7 +593,7 @@ pvisitors (GJSON * json, GMetrics * nmetrics, int sp) {
 
   popen_obj_attr (json, "visitors", sp);
   /* print visitors */
-  pskeyival (json, "count", nmetrics->visitors, isp, 0);
+  pskeyu64val (json, "count", nmetrics->visitors, isp, 0);
   /* print visitors percent */
   pskeyfval (json, "percent", nmetrics->visitors_perc, isp, 1);
   pclose_obj (json, sp, 0);
@@ -709,45 +700,6 @@ pmeta_i64_data (GJSON * json, GHolder * h, void (*cb) (GModule, uint64_t *, uint
   pclose_obj (json, sp, 1);
 }
 
-static void
-pmeta_i32_data (GJSON * json, GHolder * h, void (*cb) (GModule, uint32_t *, uint32_t *),
-                const char *key, int show_perc, int sp) {
-  int isp = 0;
-  uint32_t max = 0, min = 0, total = ht_get_meta_data (h->module, key);
-  float avg = (total == 0 ? 0 : (((float) total) / h->ht_size));
-
-  /* use tabs to prettify output */
-  if (conf.json_pretty_print)
-    isp = sp + 1;
-
-  cb (h->module, &min, &max);
-
-  popen_obj_attr (json, "total", sp);
-  pskeyival (json, "value", total, isp, 1);
-  pclose_obj (json, sp, 0);
-
-  popen_obj_attr (json, "avg", sp);
-  pskeyival (json, "value", avg, isp, !show_perc);
-  if (show_perc) {
-    pskeyfval (json, "percent", get_percentage (total, avg), isp, 1);
-  }
-  pclose_obj (json, sp, 0);
-
-  popen_obj_attr (json, "max", sp);
-  pskeyival (json, "value", max, isp, !show_perc);
-  if (show_perc) {
-    pskeyfval (json, "percent", get_percentage (total, max), isp, 1);
-  }
-  pclose_obj (json, sp, 0);
-
-  popen_obj_attr (json, "min", sp);
-  pskeyival (json, "value", min, isp, !show_perc);
-  if (show_perc) {
-    pskeyfval (json, "percent", get_percentage (total, min), isp, 1);
-  }
-  pclose_obj (json, sp, 1);
-}
-
 /* Write to a buffer the hits meta data object. */
 static void
 pmeta_data_unique (GJSON * json, int ht_size, int sp) {
@@ -776,7 +728,7 @@ pmeta_data_hits (GJSON * json, GHolder * h, int sp) {
     isp = sp + 1;
 
   popen_obj_attr (json, "hits", sp);
-  pmeta_i32_data (json, h, ht_get_hits_min_max, "hits", 1, isp);
+  pmeta_i64_data (json, h, ht_get_hits_min_max, "hits", 1, isp);
   pclose_obj (json, sp, 0);
 }
 
@@ -790,7 +742,7 @@ pmeta_data_visitors (GJSON * json, GHolder * h, int sp) {
     isp = sp + 1;
 
   popen_obj_attr (json, "visitors", sp);
-  pmeta_i32_data (json, h, ht_get_visitors_min_max, "visitors", 1, isp);
+  pmeta_i64_data (json, h, ht_get_visitors_min_max, "visitors", 1, isp);
   pclose_obj (json, sp, 0);
 }
 

src/commons.h
@@ -133,8 +133,8 @@ typedef struct GMetrics {
   float visitors_perc;
   float bw_perc;
 
-  uint32_t hits;
-  uint32_t visitors;
+  uint64_t hits;
+  uint64_t visitors;
 
   /* holder has a numeric value, while
    * dashboard has a displayable string value */

src/csv.c
@@ -123,22 +123,22 @@ escape_cvs_output (FILE * fp, char *s) {
 static void
 print_csv_metric_block (FILE * fp, GMetrics * nmetrics) {
   /* basic metrics */
-  fprintf (fp, "\"%" PRIu32 "\",", nmetrics->hits);
+  fprintf (fp, "\"%" PRIu64 "\",", nmetrics->hits);
   fprintf (fp, "\"%4.2f%%\",", nmetrics->hits_perc);
-  fprintf (fp, "\"%" PRIu32 "\",", nmetrics->visitors);
+  fprintf (fp, "\"%" PRIu64 "\",", nmetrics->visitors);
   fprintf (fp, "\"%4.2f%%\",", nmetrics->visitors_perc);
 
   /* bandwidth */
   if (conf.bandwidth) {
-    fprintf (fp, "\"%lld\",", (long long) nmetrics->bw.nbw);
+    fprintf (fp, "\"%" PRIu64 "\",", nmetrics->bw.nbw);
     fprintf (fp, "\"%4.2f%%\",", nmetrics->bw_perc);
   }
 
   /* time served metrics */
   if (conf.serve_usecs) {
-    fprintf (fp, "\"%lld\",", (long long) nmetrics->avgts.nts);
-    fprintf (fp, "\"%lld\",", (long long) nmetrics->cumts.nts);
-    fprintf (fp, "\"%lld\",", (long long) nmetrics->maxts.nts);
+    fprintf (fp, "\"%" PRIu64 "\",", nmetrics->avgts.nts);
+    fprintf (fp, "\"%" PRIu64 "\",", nmetrics->cumts.nts);
+    fprintf (fp, "\"%" PRIu64 "\",", nmetrics->maxts.nts);
   }
 
   /* request method */
@@ -217,7 +217,8 @@ print_csv_summary (FILE * fp) {
   char now[DATE_TIME];
   char *source = NULL;
   const char *fmt;
-  int i = 0, total = 0;
+  int i = 0;
+  uint64_t total = 0;
   uint32_t t = 0;
 
   generate_time ();
@@ -228,12 +229,12 @@ print_csv_summary (FILE * fp) {
   fprintf (fp, fmt, i++, GENER_ID, now, OVERALL_DATETIME);
 
   /* total requests */
-  fmt = "\"%d\",,\"%s\",,,,,,,,\"%d\",\"%s\"\r\n";
+  fmt = "\"%d\",,\"%s\",,,,,,,,\"%" PRIu64 "\",\"%s\"\r\n";
   total = ht_get_processed ();
   fprintf (fp, fmt, i++, GENER_ID, total, OVERALL_REQ);
 
   /* valid requests */
-  fmt = "\"%d\",,\"%s\",,,,,,,,\"%d\",\"%s\"\r\n";
+  fmt = "\"%d\",,\"%s\",,,,,,,,\"%" PRIu64 "\",\"%s\"\r\n";
   total = ht_sum_valid ();
   fprintf (fp, fmt, i++, GENER_ID, total, OVERALL_VALID);
 
@@ -247,7 +248,7 @@ print_csv_summary (FILE * fp) {
   fprintf (fp, fmt, i++, GENER_ID, t, OVERALL_GENTIME);
 
   /* visitors */
-  fmt = "\"%d\",,\"%s\",,,,,,,,\"%d\",\"%s\"\r\n";
+  fmt = "\"%d\",,\"%s\",,,,,,,,\"%" PRIu64 "\",\"%s\"\r\n";
   total = ht_get_size_uniqmap (VISITORS);
   fprintf (fp, fmt, i++, GENER_ID, total, OVERALL_VISITORS);
 
@@ -276,7 +277,7 @@ print_csv_summary (FILE * fp) {
   fprintf (fp, fmt, i++, GENER_ID, (intmax_t) get_log_sizes (), OVERALL_LOGSIZE);
 
   /* bandwidth */
-  fmt = "\"%d\",,\"%s\",,,,,,,,\"%lu\",\"%s\"\r\n";
+  fmt = "\"%d\",,\"%s\",,,,,,,,\"%" PRIu64 "\",\"%s\"\r\n";
   fprintf (fp, fmt, i++, GENER_ID, ht_sum_bw (), OVERALL_BANDWIDTH);
 
   /* log path */

src/gdashboard.c
@@ -823,13 +823,13 @@ render_hits (GDashModule * data, GDashRender render, int *x) {
 
   if (sel) {
     /* selected state */
-    hits = int2str (data->data[idx].metrics->hits, len);
+    hits = u642str (data->data[idx].metrics->hits, len);
     draw_header (win, hits, " %s", y, 0, w, color_selected);
     free (hits);
   } else {
     /* regular state */
     wattron (win, color->attr | COLOR_PAIR (color->pair->idx));
-    mvwprintw (win, y, *x, "%*" PRIu32 "", len, data->data[idx].metrics->hits);
+    mvwprintw (win, y, *x, "%*" PRIu64 "", len, data->data[idx].metrics->hits);
     wattroff (win, color->attr | COLOR_PAIR (color->pair->idx));
   }
 
@@ -852,13 +852,13 @@ render_visitors (GDashModule * data, GDashRender render, int *x) {
 
   if (sel) {
     /* selected state */
-    visitors = int2str (data->data[idx].metrics->visitors, len);
+    visitors = u642str (data->data[idx].metrics->visitors, len);
     draw_header (win, visitors, "%s", y, *x, w, color_selected);
     free (visitors);
   } else {
     /* regular state */
     wattron (win, color->attr | COLOR_PAIR (color->pair->idx));
-    mvwprintw (win, y, *x, "%*" PRIu32 "", len, data->data[idx].metrics->visitors);
+    mvwprintw (win, y, *x, "%*" PRIu64 "", len, data->data[idx].metrics->visitors);
     wattroff (win, color->attr | COLOR_PAIR (color->pair->idx));
   }
 

src/gdashboard.h
@@ -73,8 +73,8 @@ typedef struct GDashData_ {
 
 /* Dashboard panel meta data */
 typedef struct GDashMeta_ {
-  uint32_t max_hits;            /* maximum value on the hits column */
-  uint32_t max_visitors;        /* maximum value on the visitors column */
+  uint64_t max_hits;            /* maximum value on the hits column */
+  uint64_t max_visitors;        /* maximum value on the visitors column */
 
   /* determine the maximum metric's length of these metrics */
   /* for instance, 1022 is the max value for the hits column and its length = 4 */

src/settings.c
@@ -913,12 +913,12 @@ set_time_format_str (const char *oarg) {
 /* Determine if some global flags were set through log-format. */
 static void
 contains_specifier (void) {
-  conf.serve_usecs = conf.bandwidth = 0; /* flag */
+  conf.serve_usecs = conf.bandwidth = 0;        /* flag */
   if (!conf.log_format)
     return;
 
   if (strstr (conf.log_format, "%b"))
-    conf.bandwidth = 1;       /* flag */
+    conf.bandwidth = 1; /* flag */
   if (strstr (conf.log_format, "%D"))
     conf.serve_usecs = 1;       /* flag */
   if (strstr (conf.log_format, "%T"))
@@ -942,14 +942,14 @@ set_log_format_str (const char *oarg) {
   if (type == -1 && is_json_log_format (oarg)) {
     conf.is_json_log_format = 1;
     conf.log_format = unescape_str (oarg);
-    contains_specifier ();  /* set flag */
+    contains_specifier ();      /* set flag */
     return;
   }
 
   /* type not found, use whatever was given by the user then */
   if (type == -1) {
     conf.log_format = unescape_str (oarg);
-    contains_specifier ();  /* set flag */
+    contains_specifier ();      /* set flag */
     return;
   }
 
@@ -963,7 +963,7 @@ set_log_format_str (const char *oarg) {
     conf.is_json_log_format = 1;
 
   conf.log_format = unescape_str (fmt);
-  contains_specifier ();    /* set flag */
+  contains_specifier ();        /* set flag */
 
   /* assume we are using the default date/time formats */
   set_time_format_str (oarg);

commit f6284729bbaa27ecb54640408d848a8a044ce3b4

Added back uint32_t max/min functions to JSON/HTML outputs.
Regression from d7318ac.

src/gkhash.c
@@ -2075,6 +2075,35 @@ ht_get_keymap_list_from_key (GModule module, const char *key) {
 
 /* Iterate over all the key/value pairs for the given hash structure
  * and set the maximum and minimum values found on an integer key and
+ * integer value.
+ *
+ * Note: This are expensive calls since it has to iterate over all
+ * key-value pairs
+ *
+ * If the hash structure is empty, no values are set.
+ * On success the minimum and maximum values are set. */
+static void
+get_ii32_min_max (khash_t (ii32) * hash, uint32_t * min, uint32_t * max) {
+  khint_t k;
+  uint32_t curvalue = 0;
+  int i;
+
+  for (i = 0, k = kh_begin (hash); k != kh_end (hash); ++k) {
+    if (!kh_exist (hash, k))
+      continue;
+
+    curvalue = kh_value (hash, k);
+    if (i++ == 0)
+      *min = curvalue;
+    if (curvalue > *max)
+      *max = curvalue;
+    if (curvalue < *min)
+      *min = curvalue;
+  }
+}
+
+/* Iterate over all the key/value pairs for the given hash structure
+ * and set the maximum and minimum values found on an integer key and
  * a uint64_t value.
  *
  * Note: This are expensive calls since it has to iterate over all
@@ -2969,13 +2998,13 @@ ht_get_meta_data (GModule module, const char *key) {
  * If the hash structure is empty, no values are set.
  * On success the minimum and maximum values are set. */
 void
-ht_get_hits_min_max (GModule module, uint64_t * min, uint64_t * max) {
-  khash_t (iu64) * cache = get_hash_from_cache (module, MTRC_HITS);
+ht_get_hits_min_max (GModule module, uint32_t * min, uint32_t * max) {
+  khash_t (ii32) * cache = get_hash_from_cache (module, MTRC_HITS);
 
   if (!cache)
     return;
 
-  get_iu64_min_max (cache, min, max);
+  get_ii32_min_max (cache, min, max);
 }
 
 /* Set the maximum and minimum values found on an integer key and
@@ -2984,13 +3013,13 @@ ht_get_hits_min_max (GModule module, uint64_t * min, uint64_t * max) {
  * If the hash structure is empty, no values are set.
  * On success the minimum and maximum values are set. */
 void
-ht_get_visitors_min_max (GModule module, uint64_t * min, uint64_t * max) {
-  khash_t (iu64) * cache = get_hash_from_cache (module, MTRC_VISITORS);
+ht_get_visitors_min_max (GModule module, uint32_t * min, uint32_t * max) {
+  khash_t (ii32) * cache = get_hash_from_cache (module, MTRC_VISITORS);
 
   if (!cache)
     return;
 
-  get_iu64_min_max (cache, min, max);
+  get_ii32_min_max (cache, min, max);
 }
 
 /* Set the maximum and minimum values found on an integer key and

src/gkhash.h
@@ -351,9 +351,9 @@ void destroy_date_stores (int date);
 void free_storage (void);
 void ht_get_bw_min_max (GModule module, uint64_t * min, uint64_t * max);
 void ht_get_cumts_min_max (GModule module, uint64_t * min, uint64_t * max);
-void ht_get_hits_min_max (GModule module, uint64_t * min, uint64_t * max);
+void ht_get_hits_min_max (GModule module, uint32_t * min, uint32_t * max);
 void ht_get_maxts_min_max (GModule module, uint64_t * min, uint64_t * max);
-void ht_get_visitors_min_max (GModule module, uint64_t * min, uint64_t * max);
+void ht_get_visitors_min_max (GModule module, uint32_t * min, uint32_t * max);
 void init_pre_storage (void);
 void init_storage (void);
 void u64decode (uint64_t n, uint32_t * x, uint32_t * y);

src/json.c
@@ -700,6 +700,45 @@ pmeta_i64_data (GJSON * json, GHolder * h, void (*cb) (GModule, uint64_t *, uint
   pclose_obj (json, sp, 1);
 }
 
+static void
+pmeta_i32_data (GJSON * json, GHolder * h, void (*cb) (GModule, uint32_t *, uint32_t *),
+                const char *key, int show_perc, int sp) {
+  int isp = 0;
+  uint32_t max = 0, min = 0, total = ht_get_meta_data (h->module, key);
+  float avg = (total == 0 ? 0 : (((float) total) / h->ht_size));
+
+  /* use tabs to prettify output */
+  if (conf.json_pretty_print)
+    isp = sp + 1;
+
+  cb (h->module, &min, &max);
+
+  popen_obj_attr (json, "total", sp);
+  pskeyu64val (json, "value", total, isp, 1);
+  pclose_obj (json, sp, 0);
+
+  popen_obj_attr (json, "avg", sp);
+  pskeyu64val (json, "value", avg, isp, !show_perc);
+  if (show_perc) {
+    pskeyfval (json, "percent", get_percentage (total, avg), isp, 1);
+  }
+  pclose_obj (json, sp, 0);
+
+  popen_obj_attr (json, "max", sp);
+  pskeyu64val (json, "value", max, isp, !show_perc);
+  if (show_perc) {
+    pskeyfval (json, "percent", get_percentage (total, max), isp, 1);
+  }
+  pclose_obj (json, sp, 0);
+
+  popen_obj_attr (json, "min", sp);
+  pskeyu64val (json, "value", min, isp, !show_perc);
+  if (show_perc) {
+    pskeyfval (json, "percent", get_percentage (total, min), isp, 1);
+  }
+  pclose_obj (json, sp, 1);
+}
+
 /* Write to a buffer the hits meta data object. */
 static void
 pmeta_data_unique (GJSON * json, int ht_size, int sp) {
@@ -728,7 +767,7 @@ pmeta_data_hits (GJSON * json, GHolder * h, int sp) {
     isp = sp + 1;
 
   popen_obj_attr (json, "hits", sp);
-  pmeta_i64_data (json, h, ht_get_hits_min_max, "hits", 1, isp);
+  pmeta_i32_data (json, h, ht_get_hits_min_max, "hits", 1, isp);
   pclose_obj (json, sp, 0);
 }
 
@@ -742,7 +781,7 @@ pmeta_data_visitors (GJSON * json, GHolder * h, int sp) {
     isp = sp + 1;
 
   popen_obj_attr (json, "visitors", sp);
-  pmeta_i64_data (json, h, ht_get_visitors_min_max, "visitors", 1, isp);
+  pmeta_i32_data (json, h, ht_get_visitors_min_max, "visitors", 1, isp);
   pclose_obj (json, sp, 0);
 }
 