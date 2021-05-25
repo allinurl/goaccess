caf0b80fcac56982b9dc1244a04b99209d667c8d
Christian Göttsche <cgzones@googlemail.com>
2021-05-25 19:19:16 +0200

    Free memory on return
    leaked_storage: Variable nkey going out of scope leaks the storage it points to.
    
    Found by Coverity

--- src/persistence.c
+++ src/persistence.c
@@ -338,8 +338,10 @@ migrate_unique_key (char *key) {
 
   nkey = xstrdup ("");
   while ((ptr = strchr (key, '|'))) {
-    if (!(token = extract_by_delim (&key, "|")))
+    if (!(token = extract_by_delim (&key, "|"))) {
+      free (nkey);
       return NULL;
+    }
 
     append_str (&nkey, token);
     append_str (&nkey, "|");
