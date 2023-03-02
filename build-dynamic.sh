40a80fc6e6134c37cb2d64ac9b78c14733143469
Adam Weinberger <adamw@adamw.org>
2023-03-02 10:22:49 -0500

    Fix spelling errors as reported by the spellcheck action
    Only comments and display strings are changed here.
    
    Notable here is the correction of referer => referrer.
    "Referrer" is the actual spelling; "referer" was a misspelling
    that made it into RFC 1945 (see 1).
    
    It makes the most sense to use "referer" when referring to the
    HTTP header, and "referrer" when referring to an actual referring
    page.
    
    [1]: https://en.wikipedia.org/wiki/HTTP_referer

--- build-dynamic.sh
+++ build-dynamic.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
-# Build dynmic linked binaries on Debian.
+# Build dynamic linked binaries on Debian.
 set -o nounset   ## set -u : exit the script if you try to use an uninitialised variable
 set -o errexit   ## set -e : exit the script if any statement returns a non-true return value
 
