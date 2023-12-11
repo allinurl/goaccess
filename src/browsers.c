9561e86b49b6902161343bebd90ef24c126b68c5
Gerardo O <goaccess@prosoftcorp.com>
2023-12-11 17:21:23 -0600

    Update browsers.c
    Fixed typo.

--- src/browsers.c
+++ src/browsers.c
@@ -48,7 +48,7 @@ static char ***browsers_hash = NULL;
 
 /* {"search string", "belongs to"} */
 static const char *browsers[][2] = {
-  /* Game systems: most of them are based off major browsers,
+  /* Game systems: most of them are based on major browsers,
    * thus they need to go before. */
   {"Xbox One", "Game Systems"},
   {"Xbox", "Game Systems"},
