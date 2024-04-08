allinurl/goaccess

commit 0dab9fb89fb0c21a7dc3d942494de6025b924508

Added Caddy to the list of pre-defined log formats.

src/ui.c
@@ -673,37 +673,53 @@ display_general (WINDOW * win, GHolder * h) {
   }
 }
 
+static char *
+set_default_string (WINDOW * win, int pos_y, int pos_x, size_t max_width, const char *str) {
+  char *s = xmalloc (max_width + 1), *tmp;
+  size_t len = 0;
+  size_t size_x = 0, size_y = 0;
+  getmaxyx (win, size_y, size_x);
+  (void) size_y;
+  size_x -= 4;
+
+  /* are we setting a default string */
+  if (!str) {
+    s[0] = '\0';
+    return s;
+  }
+
+  len = MIN (max_width, strlen (str));
+  memcpy (s, str, len);
+  s[len] = '\0';
+
+  /* is the default str length greater than input field? */
+  if (strlen (s) > size_x) {
+    tmp = xstrdup (&s[0]);
+    tmp[size_x] = '\0';
+    mvwprintw (win, pos_y, pos_x, "%s", tmp);
+    free (tmp);
+  } else {
+    mvwprintw (win, pos_y, pos_x, "%s", s);
+  }
+
+  return s;
+}
+
 /* Implement a basic framework to build a field input.
  *
  * On success, the inputted string is returned. */
 char *
-input_string (WINDOW * win, int pos_y, int pos_x, size_t max_width,
-              const char *str, int enable_case, int *toggle_case) {
-  char *s = xmalloc (max_width + 1), *tmp;
-  size_t i, c, pos = 0, x = 0, quit = 1, len = 0, size_x = 0, size_y = 0;
+input_string (WINDOW * win, int pos_y, int pos_x, size_t max_width, const char *str,
+              int enable_case, int *toggle_case) {
+  char *s = NULL, *tmp;
+  size_t i, c, pos = 0, x = 0, quit = 1, size_x = 0, size_y = 0;
 
   getmaxyx (win, size_y, size_x);
   size_x -= 4;
 
-  /* are we setting a default string */
-  if (str) {
-    len = MIN (max_width, strlen (str));
-    memcpy (s, str, len);
-    s[len] = '\0';
-
+  s = set_default_string (win, pos_y, pos_x, max_width, str);
+  if (str)
     x = pos = 0;
-    /* is the default str length greater than input field? */
-    if (strlen (s) > size_x) {
-      tmp = xstrdup (&s[0]);
-      tmp[size_x] = '\0';
-      mvwprintw (win, pos_y, pos_x, "%s", tmp);
-      free (tmp);
-    } else {
-      mvwprintw (win, pos_y, pos_x, "%s", s);
-    }
-  } else {
-    s[0] = '\0';
-  }
 
   if (enable_case)
     mvwprintw (win, size_y - 2, 1, " %s", CSENSITIVE);
@@ -1177,6 +1193,9 @@ set_formats (char *date_format, char *log_format, char *time_format) {
     conf.log_format = unescape_str (log_format);
   }
 
+  if (is_json_log_format (conf.log_format))
+    conf.is_json_log_format = 1;
+
   set_spec_date_format ();
 
   return NULL;
@@ -1269,6 +1288,7 @@ render_confdlg (Logs * logs, GSpinner * spinner) {
     "Google Cloud Storage",
     "AWS Elastic Load Balancing (HTTP/S)",
     "AWS Simple Storage Service (S3)",
+    "CADDY JSON Structured",
   };
   n = ARRAY_SIZE (choices);
   getmaxyx (stdscr, y, x);
@@ -1325,9 +1345,9 @@ render_confdlg (Logs * logs, GSpinner * spinner) {
         log_format = get_selected_format_str (i);
         time_format = get_selected_time_str (i);
 
-        mvwprintw (win, 12, 1, " %s", log_format);
-        mvwprintw (win, 15, 1, " %s", date_format);
-        mvwprintw (win, 18, 1, " %s", time_format);
+        free (set_default_string (win, 12, 2, CONF_MENU_W, log_format));
+        free (set_default_string (win, 15, 2, CONF_MENU_W, date_format));
+        free (set_default_string (win, 18, 2, CONF_MENU_W, time_format));
         break;
       }
       break;
@@ -1340,7 +1360,7 @@ render_confdlg (Logs * logs, GSpinner * spinner) {
       if (!log_format)
         log_format = get_input_log_format ();
 
-      cstm_log = input_string (win, 12, 2, 70, log_format, 0, 0);
+      cstm_log = input_string (win, 12, 2, strlen (log_format), log_format, 0, 0);
       if (cstm_log != NULL && *cstm_log != '\0') {
         if (log_format)
           free (log_format);

config/goaccess.conf
@@ -47,6 +47,7 @@
 #date-format %f
 
 # Squid native log format
+# Caddy
 #
 #date-format %s
 
@@ -96,6 +97,9 @@
 # Kubernetes Nginx Ingress Log Format
 #log-format %^ %^ [%h] %^ %^ [%d:%t %^] "%r" %s %b "%R" "%u" %^ %^ [%v] %^:%^ %^ %T %^ %^
 
+# CADDY JSON Structured
+#log-format {ts:"%x.%^",request:{remote_addr:"%h:%^",proto:"%H",method:"%m",host:"%v",uri:"%U",headers:{"User-Agent":["%u","%^"]},tls:{cipher_suite:"%k",proto:"%K"}},duration:"%T",size:"%b",status:"%s",resp_headers:{"Content-Type":["%M;%^"]}}
+
 # In addition to specifying the raw log/date/time formats, for
 # simplicity, any of the following predefined log format names can be
 # supplied to the log/date/time-format variables. GoAccess  can  also
@@ -112,6 +116,7 @@
 #log-format CLOUDSTORAGE
 #log-format AWSELB
 #log-format AWSS3
+#log-format CADDY
 
 ######################################
 # UI Options

src/gkhash.c
@@ -797,12 +797,12 @@ ins_ss32 (khash_t (ss32) * hash, const char *key, const char *value) {
 
   dupkey = xstrdup (key);
   k = kh_put (ss32, hash, dupkey, &ret);
-  /* operation failed, or key exists */
+  /* operation failed */
   if (ret == -1) {
     free (dupkey);
     return -1;
   }
-  /* operation failed, or key exists */
+  /* key exists */
   if (ret == 0) {
     free (dupkey);
     return 1;
@@ -2673,13 +2673,29 @@ ht_insert_hostname (const char *ip, const char *host) {
  * On success or if key exists, 0 is returned */
 int
 ht_insert_json_logfmt (GO_UNUSED void *userdata, char *key, char *spec) {
+  int ret;
+  char *dupkey = NULL;
   khash_t (ss32) * hash = ht_json_logfmt;
+  khint_t k;
 
   if (!hash)
     return -1;
 
-  if (ins_ss32 (hash, key, spec) == -1)
-    return -1;
+  k = kh_get (ss32, hash, key);
+  /* key found, free it then to insert */
+  if (k != kh_end (hash))
+    free (kh_val (hash, k));
+  else {
+    dupkey = xstrdup (key);
+    k = kh_put (ss32, hash, dupkey, &ret);
+    /* operation failed */
+    if (ret == -1) {
+      free (dupkey);
+      return -1;
+    }
+  }
+  kh_val (hash, k) = xstrdup (spec);
+
   return 0;
 }
 
@@ -3344,12 +3360,16 @@ parse_raw_data (GModule module) {
   return raw_data;
 }
 
+void
+init_pre_storage (void) {
+  ht_json_logfmt = (khash_t (ss32) *) new_ss32_ht ();
+}
+
 /* Initialize hash tables */
 void
 init_storage (void) {
   /* *INDENT-OFF* */
   ht_hostnames   = (khash_t (ss32) *) new_ss32_ht ();
-  ht_json_logfmt = (khash_t (ss32) *) new_ss32_ht ();
   ht_dates       = (khash_t (igkh) *) new_igkh_ht ();
   ht_seqs        = (khash_t (si32) *) new_si32_ht ();
   ht_cnt_overall = (khash_t (si32) *) new_si32_ht ();

src/gkhash.h
@@ -354,6 +354,7 @@ void ht_get_cumts_min_max (GModule module, uint64_t * min, uint64_t * max);
 void ht_get_hits_min_max (GModule module, uint32_t * min, uint32_t * max);
 void ht_get_maxts_min_max (GModule module, uint64_t * min, uint64_t * max);
 void ht_get_visitors_min_max (GModule module, uint32_t * min, uint32_t * max);
+void init_pre_storage (void);
 void init_storage (void);
 void u64decode (uint64_t n, uint32_t * x, uint32_t * y);
 

src/goaccess.c
@@ -1378,6 +1378,7 @@ initializer (void) {
 
   /* init random number generator */
   srand (getpid ());
+  init_pre_storage ();
 }
 
 static char *

src/settings.c
@@ -61,6 +61,7 @@ static GEnum LOGTYPE[] = {
   {"CLOUDSTORAGE" , CLOUDSTORAGE} ,
   {"AWSELB"       , AWSELB}       ,
   {"AWSS3"        , AWSS3}        ,
+  {"CADDY"        , CADDY}        ,
 };
 
 static const GPreConfLog logs = {
@@ -74,6 +75,13 @@ static const GPreConfLog logs = {
   "%^ %dT%t.%^ %v %h:%^ %^ %T %^ %^ %s %^ %b %^ \"%r\" \"%u\" %^",    /* AWS Elastic Load Balancing */
   "%^ %^ %^ %v %^: %x.%^ %~%L %h %^/%s %b %m %U",               /* Squid Native */
   "%^ %v [%d:%t %^] %h %^\"%r\" %s %^ %b %^ %L %^ \"%R\" \"%u\"", /* Amazon S3 */
+
+  /* Caddy JSON */
+  "{\"ts\":\"%x.%^\",\"request\":{\"remote_addr\":\"%h:%^\",\"proto\":\"%H\","
+    "\"method\":\"%m\",\"host\":\"%v\",\"uri\":\"%U\","
+    "\"headers\":{\"User-Agent\":[\"%u\",\"%^\"]},\"tls\":{\"cipher_suite\":\"%k\","
+    "\"proto\":\"%K\"}},\"duration\":\"%T\",\"size\":\"%b\",\"status\":\"%s\","
+    "\"resp_headers\":{\"Content-Type\":[\"%M\"]}}"
 };
 
 static const GPreConfTime times = {
@@ -377,6 +385,8 @@ get_selected_format_idx (void) {
     return SQUID;
   else if (strcmp (conf.log_format, logs.awss3) == 0)
     return AWSS3;
+  else if (strcmp (conf.log_format, logs.caddy) == 0)
+    return CADDY;
   else
     return (size_t) -1;
 }
@@ -420,6 +430,9 @@ get_selected_format_str (size_t idx) {
   case AWSS3:
     fmt = alloc_string (logs.awss3);
     break;
+  case CADDY:
+    fmt = alloc_string (logs.caddy);
+    break;
   }
 
   return fmt;
@@ -450,6 +463,7 @@ get_selected_date_str (size_t idx) {
     fmt = alloc_string (dates.usec);
     break;
   case SQUID:
+  case CADDY:
     fmt = alloc_string (dates.sec);
     break;
   }
@@ -480,6 +494,7 @@ get_selected_time_str (size_t idx) {
     fmt = alloc_string (times.usec);
     break;
   case SQUID:
+  case CADDY:
     fmt = alloc_string (times.sec);
     break;
   }
@@ -678,7 +693,7 @@ set_date_num_format (void) {
 }
 
 /* Determine if we have a valid JSON format */
-static int
+int
 is_json_log_format (const char *fmt) {
   enum json_type t = JSON_ERROR;
   json_stream json;
@@ -936,7 +951,7 @@ set_log_format_str (const char *oarg) {
     return;
   }
 
-  if (is_json_log_format (oarg))
+  if (is_json_log_format (fmt))
     conf.is_json_log_format = 1;
 
   conf.log_format = unescape_str (fmt);

src/settings.h
@@ -55,6 +55,7 @@ typedef enum LOGTYPE {
   CLOUDSTORAGE,
   AWSELB,
   AWSS3,
+  CADDY,
 } GLogType;
 
 /* predefined log times */
@@ -84,6 +85,7 @@ typedef struct GPreConfLog_ {
   const char *awselb;
   const char *squid;
   const char *awss3;
+  const char *caddy;
 } GPreConfLog;
 
 /* *INDENT-OFF* */
@@ -214,12 +216,13 @@ char *get_selected_date_str (size_t idx);
 char *get_selected_format_str (size_t idx);
 char *get_selected_time_str (size_t idx);
 const char *verify_formats (void);
+int is_json_log_format (const char *fmt);
+int parse_json_string (void *userdata, const char *str, int (*cb) (void *, char *, char *));
 size_t get_selected_format_idx (void);
 void set_date_format_str (const char *optarg);
 void set_log_format_str (const char *optarg);
 void set_spec_date_format (void);
 void set_time_format_str (const char *optarg);
-int parse_json_string (void *userdata, const char *str, int (*cb) (void *, char *, char *));
 
 extern GConf conf;
 

src/ui.h
@@ -104,11 +104,11 @@
 
 /* CONFIG DIALOG */
 #define CONF_MENU_H       6
-#define CONF_MENU_W       57
+#define CONF_MENU_W       67
 #define CONF_MENU_X       2
 #define CONF_MENU_Y       4
 #define CONF_WIN_H        20
-#define CONF_WIN_W        61
+#define CONF_WIN_W        71
 
 /* FIND DIALOG */
 #define FIND_DLG_HEIGHT   8
@@ -145,11 +145,11 @@
 
 /* CONF ERROR DIALOG */
 #define ERR_MENU_HEIGHT   10
-#define ERR_MENU_WIDTH    57
+#define ERR_MENU_WIDTH    67
 #define ERR_MENU_X        2
 #define ERR_MENU_Y        4
 #define ERR_WIN_HEIGHT    15
-#define ERR_WIN_WIDTH     61
+#define ERR_WIN_WIDTH     71
 
 #include "color.h"
 #include "commons.h"

commit b751f29233a8cc610aac94020a8c86d731650117

Resolved regression in 0dab9fb for format modification in TUI dialog.
Fixed a regression introduced in commit 0dab9fb, which previously hindered the
ability to modify log, date, and time formats within the TUI dialog.

src/ui.c
@@ -1383,9 +1383,7 @@ render_confdlg (Logs *logs, GSpinner *spinner) {
       if (!log_format)
         log_format = get_input_log_format ();
 
-      cstm_log =
-        input_string (win, 12, 2, log_format ? strlen (log_format) : CONF_MAX_LEN_DLG,
-                      log_format, 0, 0);
+      cstm_log = input_string (win, 12, 2, CONF_MAX_LEN_DLG, log_format, 0, 0);
       if (cstm_log != NULL && *cstm_log != '\0') {
         if (log_format)
           free (log_format);