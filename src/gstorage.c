allinurl/goaccess

commit 6d2cb7efae620917ccd0ba79138ce5ea2a8de483

Change GLogItem->status from char* to int
In process_log(), use array lookup instead of string compare
This will speedup process_log() by x1.38, and have x1.28 overall speedup

src/gstorage.c
@@ -1324,8 +1324,8 @@ gen_status_code_key (GKeyData *kdata, GLogItem *logitem) {
   if (!logitem->status)
     return 1;
 
-  type = verify_status_code_type (logitem->status);
   status = verify_status_code (logitem->status);
+  type = verify_status_code_type (logitem->status);
 
   get_kdata (kdata, status, status);
   get_kroot (kdata, type, type);
@@ -1390,7 +1390,7 @@ static int
 include_uniq (GLogItem *logitem) {
   int u = conf.client_err_to_unique_count;
 
-  if (!logitem->status || logitem->status[0] != '4' || (u && logitem->status[0] == '4'))
+  if (!logitem->status || (logitem->status / 100) != 4 || (u && (logitem->status / 100) == '4'))
     return 1;
   return 0;
 }

src/options.c
@@ -642,7 +642,8 @@ parse_long_opt (const char *name, const char *oarg) {
 
   /* ignore status code */
   if (!strcmp ("ignore-status", name))
-    set_array_opt (oarg, conf.ignore_status, &conf.ignore_status_idx, MAX_IGNORE_STATUS);
+    if (conf.ignore_status_idx < MAX_IGNORE_STATUS)
+      conf.ignore_status[conf.ignore_status_idx++] = atoi (oarg);
 
   /* ignore static requests */
   if (!strcmp ("ignore-statics", name)) {

src/parser.c
@@ -277,7 +277,7 @@ init_log_item (GLog *glog) {
   logitem->req = NULL;
   logitem->resp_size = 0LL;
   logitem->serve_time = 0;
-  logitem->status = NULL;
+  logitem->status = 0;
   logitem->time = NULL;
   logitem->uniq_key = NULL;
   logitem->vhost = NULL;
@@ -336,8 +336,6 @@ free_glog (GLogItem *logitem) {
     free (logitem->req_key);
   if (logitem->req != NULL)
     free (logitem->req);
-  if (logitem->status != NULL)
-    free (logitem->status);
   if (logitem->time != NULL)
     free (logitem->time);
   if (logitem->uniq_key != NULL)
@@ -900,7 +898,6 @@ parse_specifier (GLogItem *logitem, const char **str, const char *p, const char 
   char *pch, *sEnd, *bEnd, *tkn = NULL;
   double serve_secs = 0.0;
   uint64_t bandw = 0, serve_time = 0;
-  long status = 0L;
   int dspc = 0, fmtspcs = 0;
 
   errno = 0;
@@ -1109,19 +1106,14 @@ parse_specifier (GLogItem *logitem, const char **str, const char *p, const char 
     if (!(tkn = parse_string (&(*str), end, 1)))
       return spec_err (logitem, ERR_SPEC_TOKN_NUL, *p, NULL);
 
-    /* do not validate HTTP status code */
-    if (conf.no_strict_status) {
-      logitem->status = tkn;
-      break;
-    }
-
-    status = strtol (tkn, &sEnd, 10);
-    if (tkn == sEnd || *sEnd != '\0' || errno == ERANGE || status < 100 || status > 599) {
+    logitem->status = strtol (tkn, &sEnd, 10);
+    if (tkn == sEnd || *sEnd != '\0' || errno == ERANGE ||
+        (!conf.no_strict_status && (logitem->status < 100 || logitem->status > 599))) {
       spec_err (logitem, ERR_SPEC_TOKN_INV, *p, tkn);
       free (tkn);
       return 1;
     }
-    logitem->status = tkn;
+    free (tkn);
     break;
     /* size of response in bytes - excluding HTTP headers */
   case 'b':
@@ -1628,12 +1620,14 @@ is_static (const char *req) {
  * If the status code is not within the ignore-array, 0 is returned.
  * If the status code is within the ignore-array, 1 is returned. */
 static int
-ignore_status_code (const char *status) {
+ignore_status_code (int status) {
   if (!status || conf.ignore_status_idx == 0)
     return 0;
 
-  if (str_inarray (status, conf.ignore_status, conf.ignore_status_idx) != -1)
-    return 1;
+  for (int i=0; i<conf.ignore_status_idx; i++)
+    if (status == conf.ignore_status[i])
+      return 1;
+
   return 0;
 }
 
@@ -1654,13 +1648,11 @@ ignore_static (const char *req) {
  * If the request is a 404, 1 is returned. */
 static int
 is_404 (GLogItem *logitem) {
-  if (!logitem->status || *logitem->status == '\0')
-    return 0;
   /* is this a 404? */
-  if (!memcmp (logitem->status, "404", 3))
+  if (logitem->status == 404)
     return 1;
   /* treat 444 as 404? */
-  else if (!memcmp (logitem->status, "444", 3) && conf.code444_as_404)
+  else if (logitem->status == 444 && conf.code444_as_404)
     return 1;
   return 0;
 }

src/parser.h
@@ -82,7 +82,7 @@ typedef struct GLogItem_ {
   char *ref;
   char *req;
   char *req_key;
-  char *status;
+  int status;
   char *time;
   char *uniq_key;
   char *vhost;

src/settings.h
@@ -103,7 +103,7 @@ typedef struct GConf_
   const char *ignore_ips[MAX_IGNORE_IPS];       /* array of ips to ignore */
   const char *ignore_panels[TOTAL_MODULES];     /* array of panels to ignore */
   const char *ignore_referers[MAX_IGNORE_REF];  /* referrers to ignore */
-  const char *ignore_status[MAX_IGNORE_STATUS]; /* status to ignore */
+  int ignore_status[MAX_IGNORE_STATUS]; /* status to ignore */
   const char *output_formats[MAX_OUTFORMATS];   /* output format, e.g. , HTML */
   const char *sort_panels[TOTAL_MODULES];       /* sorting options for each panel */
   const char *static_files[MAX_EXTENSIONS];     /* static extensions */

src/util.c
@@ -57,80 +57,37 @@
 #include "xmalloc.h"
 
 /* HTTP status codes categories */
-static const char *code_type[][2] = {
-  {"1", STATUS_CODE_1XX},
-  {"2", STATUS_CODE_2XX},
-  {"3", STATUS_CODE_3XX},
-  {"4", STATUS_CODE_4XX},
-  {"5", STATUS_CODE_5XX},
+static const char *code_type[] = {
+  NULL,
+  STATUS_CODE_1XX,
+  STATUS_CODE_2XX,
+  STATUS_CODE_3XX,
+  STATUS_CODE_4XX,
+  STATUS_CODE_5XX,
 };
 
 /* HTTP status codes */
-static const char *codes[][2] = {
-  {"100", STATUS_CODE_100},
-  {"101", STATUS_CODE_101},
-  {"200", STATUS_CODE_200},
-  {"201", STATUS_CODE_201},
-  {"202", STATUS_CODE_202},
-  {"203", STATUS_CODE_203},
-  {"204", STATUS_CODE_204},
-  {"205", STATUS_CODE_205},
-  {"206", STATUS_CODE_206},
-  {"207", STATUS_CODE_207},
-  {"208", STATUS_CODE_208},
-  {"300", STATUS_CODE_300},
-  {"301", STATUS_CODE_301},
-  {"302", STATUS_CODE_302},
-  {"303", STATUS_CODE_303},
-  {"304", STATUS_CODE_304},
-  {"305", STATUS_CODE_305},
-  {"307", STATUS_CODE_307},
-  {"308", STATUS_CODE_308},
-  {"400", STATUS_CODE_400},
-  {"401", STATUS_CODE_401},
-  {"402", STATUS_CODE_402},
-  {"403", STATUS_CODE_403},
-  {"404", STATUS_CODE_404},
-  {"405", STATUS_CODE_405},
-  {"406", STATUS_CODE_406},
-  {"407", STATUS_CODE_407},
-  {"408", STATUS_CODE_408},
-  {"409", STATUS_CODE_409},
-  {"410", STATUS_CODE_410},
-  {"411", STATUS_CODE_411},
-  {"412", STATUS_CODE_412},
-  {"413", STATUS_CODE_413},
-  {"414", STATUS_CODE_414},
-  {"415", STATUS_CODE_415},
-  {"416", STATUS_CODE_416},
-  {"417", STATUS_CODE_417},
-  {"418", STATUS_CODE_418},
-  {"421", STATUS_CODE_421},
-  {"422", STATUS_CODE_422},
-  {"423", STATUS_CODE_423},
-  {"424", STATUS_CODE_424},
-  {"426", STATUS_CODE_426},
-  {"428", STATUS_CODE_428},
-  {"429", STATUS_CODE_429},
-  {"431", STATUS_CODE_431},
-  {"444", STATUS_CODE_444},
-  {"451", STATUS_CODE_451},
-  {"494", STATUS_CODE_494},
-  {"495", STATUS_CODE_495},
-  {"496", STATUS_CODE_496},
-  {"497", STATUS_CODE_497},
-  {"499", STATUS_CODE_499},
-  {"500", STATUS_CODE_500},
-  {"501", STATUS_CODE_501},
-  {"502", STATUS_CODE_502},
-  {"503", STATUS_CODE_503},
-  {"504", STATUS_CODE_504},
-  {"505", STATUS_CODE_505},
-  {"520", STATUS_CODE_520},
-  {"521", STATUS_CODE_521},
-  {"522", STATUS_CODE_522},
-  {"523", STATUS_CODE_523},
-  {"524", STATUS_CODE_524}
+static const char *codes[1000] = {
+  [100]=STATUS_CODE_100, STATUS_CODE_101,
+  [200]=STATUS_CODE_200, STATUS_CODE_201, STATUS_CODE_202, STATUS_CODE_203, STATUS_CODE_204,
+  [205]=STATUS_CODE_205, STATUS_CODE_206, STATUS_CODE_207, STATUS_CODE_208,
+  [300]=STATUS_CODE_300, STATUS_CODE_301, STATUS_CODE_302, STATUS_CODE_303, STATUS_CODE_304,
+  [305]=STATUS_CODE_305, NULL, STATUS_CODE_307, STATUS_CODE_308,
+  [400]=STATUS_CODE_400, STATUS_CODE_401, STATUS_CODE_402, STATUS_CODE_403, STATUS_CODE_404,
+  [405]=STATUS_CODE_405, STATUS_CODE_406, STATUS_CODE_407, STATUS_CODE_408, STATUS_CODE_409,
+  [410]=STATUS_CODE_410, STATUS_CODE_411, STATUS_CODE_412, STATUS_CODE_413, STATUS_CODE_414,
+  [415]=STATUS_CODE_415, STATUS_CODE_416, STATUS_CODE_417, STATUS_CODE_418, NULL,
+  [420]=NULL, STATUS_CODE_421, STATUS_CODE_422, STATUS_CODE_423, STATUS_CODE_424,
+  [425]=NULL, STATUS_CODE_426, NULL, STATUS_CODE_428, STATUS_CODE_429,
+  [431]=STATUS_CODE_431,
+  [444]=STATUS_CODE_444,
+  [451]=STATUS_CODE_451,
+  [494]=STATUS_CODE_494,
+  [495]=STATUS_CODE_495, STATUS_CODE_496, STATUS_CODE_497, NULL, STATUS_CODE_499,
+  [500]=STATUS_CODE_500, STATUS_CODE_501, STATUS_CODE_502, STATUS_CODE_503, STATUS_CODE_504,
+  [505]=STATUS_CODE_505,
+  [520]=STATUS_CODE_520, STATUS_CODE_521, STATUS_CODE_522, STATUS_CODE_523, STATUS_CODE_524,
+  [999]=NULL
 };
 
 /* Return part of a string
@@ -745,13 +702,11 @@ file_size (const char *filename) {
  * If not found, "Unknown" is returned.
  * On success, the status code type/category is returned. */
 const char *
-verify_status_code_type (const char *str) {
-  size_t i;
-  for (i = 0; i < ARRAY_SIZE (code_type); i++)
-    if (strchr (code_type[i][0], str[0]) != NULL)
-      return _(code_type[i][1]);
+verify_status_code_type (int code) {
+  if (code < 100 || code > 599 || code_type[code / 100] == NULL)
+    return "Unknown";
 
-  return "Unknown";
+  return code_type[code / 100];
 }
 
 /* Determine if the given status code is within the list of status
@@ -760,13 +715,11 @@ verify_status_code_type (const char *str) {
  * If not found, "Unknown" is returned.
  * On success, the status code is returned. */
 const char *
-verify_status_code (char *str) {
-  size_t i;
-  for (i = 0; i < ARRAY_SIZE (codes); i++)
-    if (strstr (str, codes[i][0]) != NULL)
-      return _(codes[i][1]);
+verify_status_code (int code) {
+  if (code < 100 || code > 599 || codes[code] == NULL)
+    return "Unknown";
 
-  return "Unknown";
+  return codes[code];
 }
 
 /* Checks if the given string is within the given array.

src/util.h
@@ -84,8 +84,8 @@ char *u322str (uint32_t d, int width);
 char *u642str (uint64_t d, int width);
 char *unescape_str (const char *src);
 char *usecs_to_str (unsigned long long usec);
-const char *verify_status_code (char *str);
-const char *verify_status_code_type (const char *str);
+const char *verify_status_code (int code);
+const char *verify_status_code_type (int code);
 int convert_date (char *res, const char *data, const char *from, const char *to, int size);
 int count_matches (const char *s1, char c);
 int find_output_type (char **filename, const char *ext, int alloc);

commit edf8a85cb5310394ee5868d267bec8433611ca62

Fixed bug with incorrectly checked 4xx status codes when `--4xx-to-unique-count` flag is specified.
The bug was introduced in commit 6d2cb7e.

src/gstorage.c
@@ -1388,7 +1388,7 @@ static int
 include_uniq (GLogItem *logitem) {
   int u = conf.client_err_to_unique_count;
 
-  if (!logitem->status || (logitem->status / 100) != 4 || (u && (logitem->status / 100) == '4'))
+  if (!logitem->status || (logitem->status / 100) != 4 || (u && (logitem->status / 100) == 4))
     return 1;
   return 0;
 }