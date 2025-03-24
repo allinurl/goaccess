allinurl/goaccess

commit 6de2cb9de53b4ad6614a9fea09390b709e2267fc

Adjust positioning of menu buttons

resources/css/app.css
@@ -103,15 +103,15 @@ nav.active .nav-list {
     nav .nav-ws-status {
 	left: 0;
 	position: fixed;
-	top: 0;
+	top: 1.5rem;
 	transition: opacity .2s;
     }
     nav .nav-gears {
-	top: 64px;
+	top: 7.9rem;
     }
     nav .nav-ws-status {
 	left: 46px;
-    top: 108px;
+    top: 12.3rem;
     }
     nav.active .nav-list {
         margin-block-start: 0;

commit ae3f66d238d9e20e01a866f2734a8d2836a60248

Fixed navigation issues on small screens and resolved regression from commit 6de2cb9.

resources/css/app.css
@@ -22,11 +22,6 @@ h3 {
 	justify-content: space-between;
 	flex-wrap: wrap;
 	gap: 0.5rem;
-	align-items: center;
-}
-.page-header #last-updated {
-    margin: 0;
-    font-size: 1.35rem;
 }
 .pagination {
 	margin: 5px 0;
@@ -68,102 +63,116 @@ h3 {
 
 /* NAVIGATION */
 nav {
-	-webkit-transition: left .2s;
 	background: #1C1C1C;
 	border-color: #5bc0de;
 	border-style: solid;
-	border-width: 0;
-	border-block-end-width: 3px;
+	border-width: 0 3px 3px 0;
+	/* Combined border properties */
+	height: 100%;
+	left: -236px;
+	overflow: hidden;
+	position: fixed;
+	top: 0;
+	width: 300px;
+	z-index: 2;
 	transition: left .2s;
 }
+
+/* Navigation List */
 nav .nav-list {
 	visibility: hidden;
 	display: none;
+	position: absolute;
+	bottom: 0;
+	left: 0;
+	right: -17px;
+	top: 0;
+	overflow-y: scroll;
 }
 
+/* Show Navigation List on Active */
 nav.active .nav-list {
-    margin-block-start: -64px;
-    visibility: visible;
-    display: block;
+	visibility: visible;
+	display: block;
 }
+
+/* Media Query for Large Screens */
 @media only screen and (min-width: 993px) {
-    nav {
-    	border-width: 0;
-    	border-inline-end-width: 3px;
-    	height: 100%;
-    	left: -236px;
-    	overflow: hidden;
-    	position: fixed;
-    	top: 0;
-    	width: 300px;
-    	z-index: 2;
-    }
-    nav .nav-bars,
-    nav .nav-gears,
-    nav .nav-ws-status {
-	left: 0;
-	position: fixed;
-	top: 1.5rem;
-	transition: opacity .2s;
-    }
-    nav .nav-gears {
-	top: 7.9rem;
-    }
-    nav .nav-ws-status {
-	left: 46px;
-    top: 12.3rem;
-    }
-    nav.active .nav-list {
-        margin-block-start: 0;
-       	bottom: 0;
-        left: 0;
-        display: block;
-        overflow-y: scroll;
-        position: absolute;
-        right: -17px;
-        top: 0;
-    }
+
+	nav .nav-bars,
+	nav .nav-gears,
+	nav .nav-ws-status {
+		left: 0;
+		position: fixed;
+		top: 1.5rem;
+		transition: opacity .2s;
+	}
+
+	nav .nav-gears {
+		top: 7.9rem;
+	}
+
+	nav .nav-ws-status {
+		left: 38px;
+		top: 12.3rem;
+	}
+
+	nav.active .nav-list {
+		margin-block-start: 0;
+	}
 }
+
+/* Navigation Header */
 nav header {
 	margin: 40px 20px 30px;
 }
+
 nav header a {
-	color: rgba(240,240,240,.7);
+	color: rgba(240, 240, 240, .7);
 	font-size: 2.7em;
 	font-weight: 300;
 	text-transform: uppercase;
 }
+
 nav header a:hover {
 	color: #eee;
 }
+
+/* Active Navigation */
 nav.active {
 	display: block !important;
 	left: 0;
 	opacity: .97;
 }
+
 nav.active .nav-bars,
 nav.active .nav-gears,
 nav.active .nav-ws-status {
 	opacity: 0;
 	visibility: hidden;
 }
+
+/* Navigation Icons */
 nav .nav-bars,
 nav .nav-gears,
 nav .nav-ws-status {
-	-webkit-transition: opacity .2s;
 	background: none;
 	border: none;
 	color: #9E9E9E;
 	font-size: 36px;
-	height: 64px;
+	height: 60px;
+	width: 60px;
 	transition: opacity .2s;
-	width: 64px;
 }
+
 nav .nav-bars:hover,
 nav .nav-gears:hover {
 	color: gainsboro;
 }
-nav .nav-ws-status {
+
+/* WebSocket Status */
+nav .nav-ws-status,
+.nav-ws-status.mini {
 	color: crimson;
 	cursor: help;
 	display: none;
@@ -172,12 +181,21 @@ nav .nav-ws-status {
 	width: 12px;
 }
 
+.nav-ws-status.mini {
+	top: 14px;
+	left: 40px;
+	position: absolute;
+}
+
 .nav-ws-status.fa-circle {
 	color: #5DB56A;
 }
+
+/* Navigation List Items */
 nav li {
 	position: relative;
 }
+
 nav li .toggle-panel {
 	cursor: pointer;
 	opacity: 0;
@@ -188,24 +206,31 @@ nav li .toggle-panel {
 	transition: all .2s;
 	visibility: hidden;
 }
+
 nav li .toggle-panel i {
-	color: rgba(200,200,200,.5);
+	color: rgba(200, 200, 200, .5);
 	opacity: 0;
 }
+
 nav li .toggle-panel.active i {
 	color: #eee;
 	opacity: 1;
 }
+
+/* Show Toggle Panel in Active Navigation */
 nav.active li .toggle-panel {
 	visibility: visible;
 	opacity: 1;
 }
+
 nav.active li:hover .toggle-panel i {
 	opacity: 1;
 }
+
+/* Navigation Links */
 nav li a {
 	border-left: 3px solid transparent;
-	color: rgba(200,200,200,.5);
+	color: rgba(200, 200, 200, .5);
 	display: block;
 	font-size: smaller;
 	max-width: 235px;
@@ -217,25 +242,32 @@ nav li a {
 	transition: opacity .2s;
 	white-space: nowrap;
 }
+
 nav.active li a {
 	max-width: 90%;
 	opacity: 1;
 }
+
 nav li:hover a,
 nav li.active a {
-	background: rgba(0,0,0,.1);
+	background: rgba(0, 0, 0, .1);
 	border-color: #5BC0DE;
 	color: #eee;
 }
+
+/* Unordered List */
 nav ul {
 	padding-left: 0;
 	list-style: none;
 }
-/* Navigation  -- Icon */
+
+/* Navigation Links */
 nav a,
 nav a:hover {
 	text-decoration: none;
 }
+
+/* Navigation Section Titles */
 nav h3 {
 	color: #FFF !important;
 	font-size: medium;

resources/js/app.js
@@ -385,7 +385,7 @@ GoAccess.OverallStats = {
 	renderData: function (data, ui) {
 		var idx = 0, row = null;
 
-		$('#last-updated span').innerHTML = data.date_time;
+		$('.last-updated').innerHTML = data.date_time;
 		$('#overall').innerHTML = '';
 
 		if (GoAccess.Util.isPanelHidden('general'))
@@ -425,6 +425,11 @@ GoAccess.Nav = {
 		}.bind(this);
 
 		$('.nav-gears').onclick = function (e) {
+			e.stopPropagation();
+			this.renderOpts(e);
+		}.bind(this);
+
+		$('.nav-minibars').onclick = function (e) {
 			e.stopPropagation();
 			this.renderOpts(e);
 		}.bind(this);

src/output.c
@@ -287,20 +287,34 @@ static void
 print_html_body (FILE * fp, const char *now)
 {
   fprintf (fp,
-  "<nav class='hide'></nav>"
-  "<i class='spinner fa fa-circle-o-notch fa-spin fa-3x fa-fw'></i>"
-  "<div class='container hide'>"
+  "<nav class='hidden-xs hidden-sm hide' aria-label='Main navigation'>"
+  "</nav>"
+  "<i class='spinner fa fa-circle-o-notch fa-spin fa-3x fa-fw' aria-hidden='true' aria-label='Loading'></i>"
+  "<div class='container hide' role='document'>"
   "<header class='page-header'>"
-  "<h1><i class='fa fa-tachometer' aria-hidden='true'></i> %s</h1>"
-  "<p id='last-updated' class='label label-info' aria-live='polite' aria-atomic'true' style='display:%s'>%s: <span>%s</span></p>", T_DASH, conf.no_html_last_updated ? "none" : "block", INFO_LAST_UPDATED, now);
-
+  "<h1 class='h-dashboard'>"
+  "<span class='hidden-xs hidden-sm'>"
+  "<i class='fa fa-tachometer' aria-hidden='true'></i> %s"
+  "</span>"
+  "<span class='visible-xs visible-sm'>"
+  "<i class='fa fa-bars nav-minibars' aria-label='Menu' role='button' tabindex='0'></i>"
+  "<i class='fa fa-circle nav-ws-status mini' aria-label='Status indicator'></i>"
+  "</span>"
+  "</h1>"
+  "<div style='margin-left: auto;'>"
+  "<h4>"
+  "<span class='label label-info' style='display:%s' id='last-updated' aria-live='polite' aria-atomic='true'>"
+  "<span class='hidden-xs'>%s: </span>"
+  "<span class='last-updated'>%s</span>"
+  "</span>"
+  "</h4>"
+  "</div>", T_DASH, conf.no_html_last_updated ? "none" : "block", INFO_LAST_UPDATED, now);
   fprintf (fp,
-  "<p class='report-title'>%s</p>"
+  "<p class='report-title' id='report-title'>%s</p>"
   "</header>"
-  "<aside id='overall'></aside>"
-  "<main id='panels'></main>"
+  "<aside id='overall' aria-labelledby='overall-heading'></aside>"
+  "<main id='panels' aria-labelledby='report-title'></main>"
   "</div>", conf.html_report_title ? conf.html_report_title : "");
-
   fprintf (fp, "%.*s", tpls_length, tpls);
 }
 